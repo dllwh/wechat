d5e353500065de6df2fee5a18ae4ecbd6b1dd059
皇族灬战狼 <duleilewuhen@163.com>
2017-04-08 15:17:18 +0800

    监听器

---
+++ wechat-manage/src/main/java/com/cdeledu/controller/system/watch/WatchMonitor.java
@@ -0,0 +1,26 @@
+package com.cdeledu.controller.system.watch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cdeledu.controller.BaseController;
+
+/**
+ * @类描述: 监听器
+ * @创建者: 皇族灬战狼
+ * @创建时间: 2017年4月8日 下午3:14:34
+ * @版本: V1.0
+ * @since: JDK 1.7
+ * @see <a href="">TODO(连接内容简介)</a>
+ */
+@Controller
+@RequestMapping("/watchMonitor")
+public class WatchMonitor  extends BaseController{
+
+	private static final long serialVersionUID = 1L;
+	/** ----------------------------------------------------- Fields start */
+	/** ----------------------------------------------------- Fields end */
+
+	/** ----------------------------------------------- [私有方法] */
+	/** ----------------------------------------------- [私有方法] */
+}
