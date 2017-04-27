0f2251ca5d44e534802597102c3355cd2e26472c
皇族灬战狼 <duleilewuhen@163.com>
2017-04-27 11:54:12 +0800

    cookies操作

--- wechat-manage/src/main/webapp/plug-in/tools/ExtJavascript.js
+++ wechat-manage/src/main/webapp/plug-in/tools/ExtJavascript.js
@@ -41,3 +41,32 @@ dllwh.currentTime = function() {
 	str += d.getSeconds() + '秒';
 	return str;
 }
+/** 设置cookie */
+dllwh.setCookie = function(name,value){
+	var exp = new Date();
+	var tommorrow=new Date(); 
+	tommorrow.setDate(tommorrow.getDate()+1); 
+	tommorrow.setHours(0);
+	tommorrow.setMinutes(0);
+	tommorrow.setSeconds(0);
+	tommorrow.setMilliseconds(0);
+	exp.setTime(tommorrow.getTime()); //明天0点过期
+	document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+}
+/** 获取cookie */
+dllwh.getCookie = function(name) {
+	var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+	if (arr = document.cookie.match(reg))
+		return unescape(arr[2]);
+	else
+		return "";
+}
+
+/** 删除cookies */
+dllwh.delCookie = function(name){
+	var exp = new Date();
+	exp.setTime(exp.getTime() - 1);
+	var cval=getCookie(name);
+	if(cval!=null)
+	document.cookie= name + "="+cval+";expires="+exp.toGMTString();
+}
