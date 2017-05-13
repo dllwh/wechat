9c9832070668b7f1669ad3185444a33ca05c6af0
皇族灬战狼 <duleilewuhen@163.com>
2017-05-13 17:30:17 +0800

    simple-line-icons

--- wechat-manage/src/main/java/com/cdeledu/service/sys/IconService.java
+++
@@ -1,8 +0,0 @@
-package com.cdeledu.service.sys;
-
-import com.cdeledu.common.base.BaseService;
-import com.cdeledu.model.system.IconResource;
-
-public interface IconService extends BaseService<IconResource> {
-
-}
