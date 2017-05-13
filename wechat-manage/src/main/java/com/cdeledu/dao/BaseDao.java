70c38539e306c7cd06e30e3d915b5ab8d391d44b
皇族灬战狼 <duleilewuhen@163.com>
2017-05-13 17:42:07 +0800

    sqlSessionTemplate 持久化框架

---
+++ wechat-manage/src/main/java/com/cdeledu/dao/BaseDao.java
@@ -0,0 +1,73 @@
+package com.cdeledu.dao;
+
+import java.util.List;
+
+public interface BaseDao {
+
+	/**
+	 * 支持Oracle序列,UUID,类似Mysql的INDENTITY自动增长(自动回写)
+	 */
+	public Integer insert(String statement, Object parameter) throws Exception;
+
+	/**
+	 * 根据实体类中字段不为null的条件进行删除,条件全部使用=号and条件
+	 */
+	public Integer delete(String statement, Object parameter) throws Exception;
+
+	/**
+	 * 根据主键进行更新,这里最多只会更新一条数据
+	 */
+	public Integer update(String statement, Object parameter) throws Exception;
+
+	/**
+	 * @方法描述: 批量插入
+	 */
+	public Integer batchInsert(String statement, List<Object> parameter) throws Exception;
+
+	/**
+	 * @方法描述: 批量更新
+	 */
+	public Integer batchUpdate(String statement, List<Object> parameter) throws Exception;
+
+	/**
+	 * @方法描述: 批量删除
+	 */
+	public Integer batchDelete(String statement, List<Object> parameter) throws Exception;
+
+	/**
+	 * 通过JDBC查找对象集合 使用指定的检索标准检索数据返回数据
+	 */
+	public Object findOneForJdbc(String statement, Object parameters) throws Exception;
+
+	/**
+	 * @方法描述: @方法描述: 查找对象封装成Map
+	 * @param statement
+	 *            执行的语句
+	 * @param obj
+	 *            参数
+	 * @param key
+	 *            所返集合的key;此名字与查询语句中的列名对应,表示已那个字段作为key
+	 * @param value
+	 *            所返集合的value,此名字与查询语句中的列名对应,表示已那个字段作为value
+	 * @return
+	 * @throws Exception
+	 */
+	public Object findOneForMap(String statement, Object obj, String key, String value)
+			throws Exception;
+
+	/**
+	 * 通过JDBC查找对象集合,带分页 使用指定的检索标准检索数据并分页返回数据
+	 */
+	public List<Object> findForJdbcParam(String statement, Object parameter) throws Exception;
+
+	/**
+	 * @方法描述: 查找对象
+	 */
+	public Object findForList(String statement, Object obj) throws Exception;
+
+	/**
+	 * 使用指定的检索标准检索数据并分页返回数据For JDBC-采用预处理方式
+	 */
+	public Integer getCountForJdbcParam(String statement, Object parameter) throws Exception;
+
+}
---
+++ wechat-manage/src/main/java/com/cdeledu/dao/BaseDaoSupport.java
@@ -0,0 +1,61 @@
+package com.cdeledu.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BaseDaoSupport implements BaseDao {
+
+	@Resource(name = "sqlSessionTemplate")
+	private SqlSessionTemplate sqlSessionTemplate;
+
+	public Integer insert(String statement, Object parameter) throws Exception {
+		return sqlSessionTemplate.insert(statement,parameter);
+	}
+
+	public Integer delete(String statement, Object parameter) throws Exception {
+		return sqlSessionTemplate.delete(statement, parameter);
+	}
+
+	public Integer update(String statement, Object parameter) throws Exception {
+		return sqlSessionTemplate.update(statement, parameter);
+	}
+
+	public Integer batchInsert(String statement, List<Object> parameter) throws Exception {
+		return sqlSessionTemplate.insert(statement);
+	}
+
+	public Integer batchUpdate(String statement, List<Object> parameter) throws Exception {
+		return null;
+	}
+
+	public Integer batchDelete(String statement, List<Object> parameter) throws Exception {
+		return sqlSessionTemplate.delete(statement, parameter);
+	}
+
+	public Object findOneForJdbc(String statement, Object parameter) throws Exception {
+		return sqlSessionTemplate.selectOne(statement, parameter);
+	}
+
+	public Object findOneForMap(String statement, Object obj, String key, String value)
+			throws Exception {
+		return sqlSessionTemplate.selectMap(statement, obj, key);
+	}
+
+	public List<Object> findForJdbcParam(String statement, Object parameter) throws Exception {
+		return sqlSessionTemplate.selectList(statement, parameter);
+	}
+
+	public Object findForList(String statement, Object obj) throws Exception {
+		return sqlSessionTemplate.selectList(statement, obj);
+	}
+
+	public Integer getCountForJdbcParam(String statement, Object parameter) throws Exception {
+		return (Integer)sqlSessionTemplate.selectOne(statement, parameter);
+	}
+
+}
