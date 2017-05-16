c179583a1bcb7f0c5cc103cda47ee17136d9a7f7
皇族灬战狼 <duleilewuhen@163.com>
2017-05-16 20:29:56 +0800

    城市数据来源

---
+++ wechat-webCrawler/src/main/java/com/cdeledu/crawler/transport/traffic/TrainHelper.java
@@ -0,0 +1,33 @@
+package com.cdeledu.crawler.transport.traffic;
+
+import java.util.List;
+
+import com.cdeledu.common.constant.ConstantHelper;
+import com.cdeledu.util.application.regex.RegexUtil;
+import com.cdeledu.util.network.tcp.HttpURLConnHelper;
+import com.google.common.collect.Lists;
+
+public class TrainHelper {
+	/** ----------------------------------------------------- Fields start */
+	/** ----------------------------------------------------- Fields end */
+
+	/** ----------------------------------------------- [私有方法] */
+	/** ----------------------------------------------- [私有方法] */
+	/**
+	 * @方法描述: 城市数据来源
+	 * @return
+	 * @throws Exception
+	 */
+	public List<String> getStationInfo() throws Exception {
+		/** 列车站点列表 */
+		String url = "https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.8968";
+		String result = HttpURLConnHelper.sendGetRequest(url, ConstantHelper.UTF_8);
+		List<String> resultList = Lists.newArrayList();
+		/** 简称|车站名字|车站代码|城市拼音|简称|排序 */
+		// String regex =
+		// "(@([a-z]+))\\|(([\u4e00-\u9fa5\uF900-\uFA2D\u0020])+)\\|([A-Z]+)\\|([a-z]+)\\|([a-z]+)\\|(\\d*)";
+		String regex = "(([\u4e00-\u9fa5\uF900-\uFA2D\u0020])+)\\|([A-Z]+)\\|([a-z]+)\\|([a-z]+)\\|(\\d*)";
+		RegexUtil.getList(regex, result, 0, resultList);
+		return resultList;
+	}
+}
