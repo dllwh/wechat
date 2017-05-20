5fb8f844286f7817372af0572adecce793fa23ee
皇族灬战狼 <duleilewuhen@163.com>
2017-05-20 11:34:07 +0800

    修改

--- wechat-manage/src/main/java/com/cdeledu/service/impl/sys/ManagerUserServiceImpl.java
+++ wechat-manage/src/main/java/com/cdeledu/service/impl/sys/ManagerUserServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cdeledu.common.base.BaseClass;
-import com.cdeledu.dao.ManagerUserDao;
+import com.cdeledu.dao.BaseDaoSupport;
 import com.cdeledu.model.rbac.ManagerUser;
 import com.cdeledu.model.rbac.ManagerUserRole;
 import com.cdeledu.service.sys.ManagerUserService;
@@ -21,97 +21,110 @@ import com.cdeledu.util.security.PasswordUtil;
  * @版本: V1.0
  * @since: JDK 1.7
  */
-@Service
-@Transactional(readOnly = true)
+@Service("managerUserService")
+@Transactional(readOnly = false)
 public class ManagerUserServiceImpl extends BaseClass implements ManagerUserService {
 	private static final long serialVersionUID = 1L;
+	private final static String prefix = "manageUserDaoImpl.";
 	/** ----------------------------------------------------- Fields start */
 	@Resource
-	private ManagerUserDao managerUserDao;
+	private BaseDaoSupport<?> baseDao;
 
 	/** ----------------------------------------------------- Fields end */
 	/**
 	 * 保存用户基本信息
 	 */
-	public int insertSelective(ManagerUser record) throws Exception {
+	@Transactional(readOnly = false)
+	public Integer insert(ManagerUser record) throws Exception {
 		logger.info("保存用户基本信息");
 		String newPassWord = PasswordUtil.encrypt(record.getUserName(), record.getPassword());
 		record.setPassword(newPassWord);
-		return managerUserDao.insertSelective(record);
+		return baseDao.insert(prefix + "insertSelective", record);
 	}
 
 	/**
 	 * 批量保存
 	 */
-	public int batchInsert(List<ManagerUser> record) throws Exception {
-		return managerUserDao.batchInsert(record);
+	@Transactional(readOnly = false)
+	public Integer batchInsert(List<ManagerUser> record) throws Exception {
+		return null;
 	}
 
 	/**
 	 * 删除用户
 	 */
-	public int delete(ManagerUser record) throws Exception {
-		return managerUserDao.delete(record);
+	@Transactional(readOnly = false)
+	public Integer delete(Object record) throws Exception {
+		return baseDao.delete(prefix + "delete", record);
 	}
 
-	/**
-	 * 删除用户通过主键
-	 */
-	public int deleteByPrimaryKey(Object key) throws Exception {
-		return managerUserDao.deleteByPrimaryKey(key);
+	@Transactional(readOnly = false)
+	public Integer batchDelete(List<ManagerUser> parameter) throws Exception {
+		return null;
 	}
 
 	/**
 	 * 更新用户信息
 	 */
-	public int updateByPrimaryKey(ManagerUser record) throws Exception {
-		return managerUserDao.updateByPrimaryKey(record);
+	@Transactional(readOnly = false)
+	public Integer update(ManagerUser record) throws Exception {
+		return null;
+	}
+
+	@Transactional(readOnly = false)
+	public Integer batchUpdate(List<ManagerUser> parameter) throws Exception {
+		return null;
 	}
 
 	/**
 	 * 返回用户列表
 	 */
+	@Transactional(readOnly = true)
 	public List<ManagerUser> findForJdbc(ManagerUser record) throws Exception {
-		return managerUserDao.findForJdbc(record);
+		return null;
 	}
 
 	/**
 	 * 获取用户列表的个数
 	 */
-	public int getCountForJdbcParam(ManagerUser record) throws Exception {
-		return managerUserDao.getCountForJdbcParam(record);
+	@Transactional(readOnly = true)
+	public Integer getCountForJdbcParam(ManagerUser record) throws Exception {
+		return null;
 	}
 
 	/**
 	 * 获取用户信息(单独一个)
 	 */
+	@Transactional(readOnly = true)
 	public ManagerUser findOneForJdbc(ManagerUser record) throws Exception {
-		return managerUserDao.findOneForJdbc(record);
+		return null;
 	}
 
 	/**
 	 * 检查用户是否存在
 	 */
+	@Transactional(readOnly = true)
 	public ManagerUser checkUserExits(ManagerUser user) throws Exception {
 		// 密码加密
 		String password = PasswordUtil.encrypt(user.getUserName(), user.getPassword());
 		ManagerUser managerUser = new ManagerUser();
 		managerUser.setUserName(user.getUserName());
 		managerUser.setPassword(password);
-
-		return managerUserDao.checkUserExits(managerUser);
+		return (ManagerUser) baseDao.findOneForJdbcParam(prefix + "checkUserExits", managerUser);
 	}
 
 	/**
 	 * 获取用户的角色
 	 */
+	@Transactional(readOnly = true)
 	public ManagerUserRole getUserRole(ManagerUser managerUser) throws Exception {
-		return managerUserDao.getUserRole(managerUser);
+		return null;
 	}
 
 	/**
 	 * 保存管理员登录信息
 	 */
+	@Transactional(readOnly = false)
 	public void saveLoginInfo(ManagerUser managerUser) {
 
 	}
@@ -119,12 +132,14 @@ public class ManagerUserServiceImpl extends BaseClass implements ManagerUserServ
 	/**
 	 * admin账户初始化
 	 */
+	@Transactional(readOnly = false)
 	public void pwdInit(ManagerUser managerUser) {
 	}
 
 	/**
 	 * 保存用户-角色关联关系
 	 */
+	@Transactional(readOnly = false)
 	public void saveRoleUser(ManagerUserRole managerUserRole) {
 	}
 
