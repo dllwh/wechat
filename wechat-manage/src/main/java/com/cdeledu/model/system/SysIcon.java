ad3161ba6ff09dac5d6e6720205a294440cca830
皇族灬战狼 <duleilewuhen@163.com>
2017-05-22 15:38:46 +0800

    图标资源

---
+++ wechat-manage/src/main/java/com/cdeledu/model/system/SysIcon.java
@@ -0,0 +1,72 @@
+package com.cdeledu.model.system;
+
+import java.util.Date;
+
+import com.cdeledu.common.base.BaseEntity;
+
+/**
+ * @类描述: 图标资源
+ * @创建者: 皇族灬战狼
+ * @创建时间: 2017年5月22日 下午3:33:25
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public class SysIcon extends BaseEntity<SysIcon> {
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	/** 显示名称 */
+	private String displayName;
+	/** class 名字 */
+	private String className;
+	/** 来源 */
+	private String sourceType;
+	/** */
+	private Date createTime;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getSourceType() {
+		return sourceType;
+	}
+
+	public void setSourceType(String sourceType) {
+		this.sourceType = sourceType;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	@Override
+	public String toString() {
+		return "SysIcon [displayName=" + displayName + ", className=" + className + ", sourceType="
+				+ sourceType + ", createTime=" + createTime + "]";
+	}
+
+}
