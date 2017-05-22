0308ac4335d7b639e7d820e2f4da05acbb4c59b0
皇族灬战狼 <duleilewuhen@163.com>
2017-05-22 09:04:55 +0800

    常量工具

--- wechat-common/src/main/java/com/cdeledu/common/constant/ConstantHelper.java
+++ wechat-common/src/main/java/com/cdeledu/common/constant/ConstantHelper.java
@@ -26,7 +26,21 @@ public final class ConstantHelper {
 	public static final String JAR_PATH_EXT = ".jar!";
 	/** 当Path为文件形式时, path会加入一个表示文件的前缀 */
 	public static final String PATH_FILE_PRE = "file:";
-
+	
+	/** UNDERLINE [String] 下划线 字符串 */
+	public static final String UNDERLINE = "_";
+	/** COMMA [String] 逗号 字符串 */
+	public static final String COMMA = ",";
+	/** COLON [String] 冒号 字符串 */
+	public static final String COLON = ":";
+	/** NEW_LINE [String] */
+	public static final String NEW_LINE = System.getProperty("line.separator");
+	
+	/** CALLBACK [String] 跨域访问的回调函数名 对应的参数Key值 */
+	public static final String CALLBACK = "callback";
+	/** MODEL_NAME_AJAX_JSON [Object] MODEL_NAME_AJAX_JSON */
+	public static final String MODEL_NAME_AJAX_JSON = "ajax_json";
+	
 	/**
 	 * @方法:默认编码
 	 * @创建人:独泪了无痕
