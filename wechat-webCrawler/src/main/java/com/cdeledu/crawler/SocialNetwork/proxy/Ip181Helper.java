a82ed08c7dbca7011cd585413604006b63ee9d9d
皇族灬战狼 <duleilewuhen@163.com>
2017-08-10 19:05:53 +0800

    代理ip检测平台

--- wechat-webCrawler/src/main/java/com/cdeledu/crawler/SocialNetwork/proxy/Ip181Helper.java
+++ wechat-webCrawler/src/main/java/com/cdeledu/crawler/SocialNetwork/proxy/Ip181Helper.java
@@ -1,45 +1,82 @@
 package com.cdeledu.crawler.SocialNetwork.proxy;
 
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import com.google.common.collect.Maps;
+import com.beust.jcommander.internal.Lists;
+import com.cdeledu.common.mapper.JsonMapper;
+import com.cdeledu.crawler.SocialNetwork.proxy.common.ProxyPool;
+import com.cdeledu.util.apache.collection.MapUtilHelper;
 
 public class Ip181Helper {
 	/** ----------------------------------------------------- Fields start */
 	private static final String BASE_URL = "http://ip181.com/";
+	private static final String PAGE_URL = BASE_URL + "daili/%s.html";
 
 	/** ----------------------------------------------------- Fields end */
-	public static void getProxyIP() throws Exception {
-		Document document = Jsoup.connect(BASE_URL).get();
-		Elements dateTable = document.select("div.panel-body").first().getElementsByTag("table")
+	/**
+	 * @方法描述: 获取精选代理IP
+	 * @return
+	 * @throws Exception
+	 */
+	public static String getProxyIP() throws Exception {
+		return JsonMapper.toJsonString(getProxyIPByPage(BASE_URL));
+	}
+
+	/**
+	 * @方法描述: 每日免费代理ip列表
+	 * @param pageNum
+	 * @return
+	 * @throws Exception
+	 */
+	public static String getProxyIP(int pageNum) throws Exception {
+		if (pageNum < 1) {
+			pageNum = 1;
+		}
+		return JsonMapper.toJsonString(getProxyIPByPage(String.format(PAGE_URL, pageNum)));
+	}
+
+	/**
+	 * @方法描述: 获取总的代理IP的总数，用来分页
+	 * @return
+	 */
+	public static int getProxyIPTotal() throws Exception {
+		Document document = Jsoup.connect(String.format(PAGE_URL, 1)).get();
+		String total = document.select("div.page").first().getElementsByTag("font").first().text();
+		return Integer.valueOf(total);
+	}
+
+	private static String getProxyIPByPage(String url) throws Exception {
+		List<Map<String, Object>> resultList = Lists.newArrayList();
+		Document document = Jsoup.connect(url).get();
+		Elements dataTable = document.select("div.panel-body").first().getElementsByTag("table")
 				.first().getElementsByTag("tr");
-		for (int i = 1; i < dateTable.size(); i++) {
-			getDetailInfo(dateTable.get(i));
-			System.out.println();
+		for (int i = 1; i < dataTable.size(); i++) {
+			resultList.add(MapUtilHelper.BeanToMap(getDetailInfo(dataTable.get(i))));
 		}
+		return JsonMapper.toJsonString(resultList);
 	}
 
-	private static Map<String, Object> getDetailInfo(Element date) {
-		Map<String, Object> resultMap = Maps.newConcurrentMap();
-		Elements tdDate = date.getElementsByTag("td");
-		resultMap.put("ip", tdDate.get(0).text());
-		resultMap.put("port", tdDate.get(1).text());
-		resultMap.put("type", tdDate.get(3).text().split(",").toString());
-		resultMap.put("position", tdDate.get(5).text());
-		System.out.println(resultMap);
-		return resultMap;
-	}
-
-	public static void main(String[] args) {
-		try {
-			getProxyIP();
-		} catch (Exception e) {
-			e.printStackTrace();
+	/**
+	 * @方法描述: 获取详细信息
+	 * @param data
+	 * @return
+	 */
+	private static ProxyPool getDetailInfo(Element data) {
+		ProxyPool proxyIP = new ProxyPool();
+		Elements tdData = data.getElementsByTag("td");
+		proxyIP.setIp(tdData.get(0).text());
+		if (StringUtils.isNoneBlank(tdData.get(1).text())) {
+			proxyIP.setPort(Integer.valueOf(tdData.get(1).text()));
 		}
+		proxyIP.setType(tdData.get(3).text().split(","));
+		proxyIP.setPosition(tdData.get(5).text());
+		return proxyIP;
 	}
 }
--- wechat-webCrawler/src/main/java/com/cdeledu/crawler/SocialNetwork/proxy/common/ProxyPool.java
+++ wechat-webCrawler/src/main/java/com/cdeledu/crawler/SocialNetwork/proxy/common/ProxyPool.java
@@ -1,16 +1,16 @@
 package com.cdeledu.crawler.SocialNetwork.proxy.common;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.Arrays;
 
 /**
- * @类描述: TODO(这里用一句话描述这个类的作用)
+ * @类描述: 代理ip
  * @创建者: 独泪了无痕--duleilewuhen@sina.com
  * @创建日期: 2017年8月9日 下午10:03:32
  * @版本: V1.0
  * @since: JDK 1.7
  */
-class ProxyPool implements Serializable {
+public class ProxyPool implements Serializable {
 	private static final long serialVersionUID = 1L;
 	/** ----------------------------------------------------- Fields start */
 	/** IP 地址 */
@@ -28,59 +28,77 @@ class ProxyPool implements Serializable {
 	/** 运营商 */
 	private String isp;
 	/** 代理类型 */
-	private List<String> type;
+	private String[] type;
+
+	/** ----------------------------------------------------- Fields end */
 	public String getIp() {
 		return ip;
 	}
+
 	public void setIp(String ip) {
 		this.ip = ip;
 	}
+
 	public Integer getPort() {
 		return port;
 	}
+
 	public void setPort(Integer port) {
 		this.port = port;
 	}
+
 	public String getPosition() {
 		return position;
 	}
+
 	public void setPosition(String position) {
 		this.position = position;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public String getProvince() {
 		return province;
 	}
+
 	public void setProvince(String province) {
 		this.province = province;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getIsp() {
 		return isp;
 	}
+
 	public void setIsp(String isp) {
 		this.isp = isp;
 	}
-	public List<String> getType() {
+
+	public String[] getType() {
 		return type;
 	}
-	public void setType(List<String> type) {
+
+	public void setType(String[] type) {
 		this.type = type;
 	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
 
-	/** ----------------------------------------------------- Fields end */
-	
+	@Override
+	public String toString() {
+		return "ProxyPool [ip=" + ip + ", port=" + port + ", position=" + position + ", country="
+				+ country + ", province=" + province + ", city=" + city + ", isp=" + isp + ", type="
+				+ Arrays.toString(type) + "]";
+	}
 }
