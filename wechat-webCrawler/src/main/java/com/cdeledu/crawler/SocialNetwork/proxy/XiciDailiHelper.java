1cc0508eb02357f52fac3bbb0a272e3d1631941d
皇族灬战狼 <duleilewuhen@163.com>
2017-08-18 16:04:18 +0800

    爬取西刺上的代理IP

--- wechat-webCrawler/src/main/java/com/cdeledu/crawler/SocialNetwork/proxy/XiciDailiHelper.java
+++ wechat-webCrawler/src/main/java/com/cdeledu/crawler/SocialNetwork/proxy/XiciDailiHelper.java
@@ -1,6 +1,18 @@
 package com.cdeledu.crawler.SocialNetwork.proxy;
 
-import com.cdeledu.util.network.tcp.HttpClientHelper;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import com.beust.jcommander.internal.Lists;
+import com.cdeledu.common.browser.UserAgentType;
+import com.cdeledu.common.mapper.JsonMapper;
+import com.cdeledu.crawler.SocialNetwork.proxy.entity.ProxyPool;
+import com.cdeledu.util.apache.collection.MapUtilHelper;
 
 /**
  * @类描述: API提取代理ip地址
@@ -12,31 +24,70 @@ import com.cdeledu.util.network.tcp.HttpClientHelper;
  */
 public class XiciDailiHelper {
 	/** ----------------------------------------------------- Fields start */
-	private final static String BASE_URL = "http://api.xicidaili.com/";
-	private final static String FREE_URL = BASE_URL + "free2016.txt";
+	private final static String BASE_URL = "http://www.xicidaili.com/";
 
 	/** ----------------------------------------------------- Fields end */
 
 	/** ----------------------------------------------- [私有方法] */
 	/** ----------------------------------------------- [私有方法] */
-	/**
-	 * @方法描述:
-	 * 
-	 *        <pre>
-	 * 免费API提取地址
-	 * API链接中的IP地址每15分钟更新一次，因此不建议频繁读取API，15分钟来读取一次即可。
-	 *        </pre>
-	 * 
-	 * @return
-	 */
-	public static String[] getProxyIpByFreeApi() {
-		String[] ip = null;
+	public static String getProxyList(String url) {
+		List<Map<String, Object>> resultList = null;
 		try {
-			String result = new HttpClientHelper().sendGetRequest(FREE_URL);
-			ip = result.split("\r\n");
+			resultList = Lists.newArrayList();
+			Document document = Jsoup.connect(url)
+					.header("User-Agent", UserAgentType.Mobile_Firefox.name()).get();
+			Elements dataTable = document.body().select("table#ip_list").first().select("tr");
+			ProxyPool proxyIP = null;
+			for (int i = 1; i < dataTable.size(); i++) {
+				try {
+					proxyIP = new ProxyPool();
+					Elements tdData = dataTable.get(i).getElementsByTag("td");
+					proxyIP.setIp(tdData.get(1).text());
+					if (StringUtils.isNoneBlank(tdData.get(2).text())) {
+						proxyIP.setPort(Integer.valueOf(tdData.get(2).text()));
+					}
+					proxyIP.setPosition(tdData.get(3).text());
+					proxyIP.setProtocolType(tdData.get(5).text().toLowerCase().split(","));
+					resultList.add(MapUtilHelper.BeanToMap(proxyIP));
+				} catch (Exception e) {
+				}
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		return ip;
+		return JsonMapper.toJsonString(resultList);
 	}
+
+	/**
+	 * @方法描述: 国内高匿代理
+	 * @return
+	 */
+	public static String getProxyListBySecret() {
+		return getProxyList(BASE_URL + "nn/");
+	}
+
+	/**
+	 * @方法描述: 国内透明代理
+	 * @return
+	 */
+	public static String getProxyListByTransparent() {
+		return getProxyList(BASE_URL + "nt/");
+	}
+
+	/**
+	 * @方法描述: 国内HTTPS代理
+	 * @return
+	 */
+	public static String getProxyListByHttps() {
+		return getProxyList(BASE_URL + "wn/");
+	}
+
+	/**
+	 * @方法描述: 国内HTTP代理
+	 * @return
+	 */
+	public static String getProxyListByHttp() {
+		return getProxyList(BASE_URL + "wt/");
+	}
+
 }
