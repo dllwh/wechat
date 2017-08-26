5a93c0f0b8e398d78f8d0fb07c0e3e1e15ab88a1
皇族灬战狼 <duleilewuhen@163.com>
2017-08-26 16:53:29 +0800

    私密代理

---
+++ wechat-webCrawler/src/main/java/com/cdeledu/crawler/SocialNetwork/proxy/KuaidlProxyHelper.java
@@ -0,0 +1,14 @@
+package com.cdeledu.crawler.SocialNetwork.proxy;
+/**
+ * @类描述: TODO(这里用一句话描述这个类的作用)
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建日期: 2017年8月20日 下午9:39:53
+ * @版本: V1.0
+ * @since: JDK 1.7
+ * @see <a href="http://www.kuaidaili.com">私密代理</a>
+ */
+public class KuaidlProxyHelper {
+	/** ----------------------------------------------------- Fields start */
+
+	/** ----------------------------------------------------- Fields end */
+}
