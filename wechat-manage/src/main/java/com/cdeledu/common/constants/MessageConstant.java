16fd5873668409fbdd639509264aa1f374f2fbd9
皇族灬战狼 <duleilewuhen@163.com>
2017-10-11 09:17:26 +0800

    常量提示语

---
+++ wechat-manage/src/main/java/com/cdeledu/common/constants/MessageConstant.java
@@ -0,0 +1,35 @@
+package com.cdeledu.common.constants;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 常量提示语
+ * @创建者: 皇族灬战狼
+ * @创建时间: 2017年10月9日 下午1:44:02
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public class MessageConstant {
+	/**
+	 * 登录成功
+	 */
+	public static final String LOGIN_SUCCESS = "LOGIN_SUCCESS";
+	/**
+	 * 用户名或密码错误
+	 */
+	public static final String LOGIN_USER_REEOE = "LOGIN_USER_REEOE";
+	/**
+	 * 用户被锁定
+	 */
+	public static final String LOGIN_USER_LOCK = "LOGIN_USER_LOCK";
+	/**
+	 * 密码错误次数已达上限
+	 */
+	public static final String LOGIN_USER_MORE = "LOGIN_USER_MORE";
+	/**
+	 * 用户审核
+	 */
+	public static final String LOGIN_USER_NOCHECK = "LOGIN_USER_NOCHECK";
+}
