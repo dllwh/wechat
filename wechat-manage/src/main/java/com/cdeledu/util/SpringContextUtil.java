479d4eea39e37d35e0a0b88bab2fd19df7caf1ec
皇族灬战狼 <duleilewuhen@163.com>
2017-11-04 19:48:33 +0800

    工具类

---
+++ wechat-manage/src/main/java/com/cdeledu/util/SpringContextUtil.java
@@ -0,0 +1,117 @@
+package com.cdeledu.util;
+
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 静态获取Bean
+ * @创建者: 皇族灬战狼
+ * @创建时间: 2017年10月31日 下午4:47:30
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public class SpringContextUtil implements ApplicationContextAware, DisposableBean {
+	/** ----------------------------------------------------- Fields start */
+	private static ApplicationContext applicationContext;
+	private static Logger logger = LoggerFactory.getLogger(SpringContextUtil.class);
+
+	/** ----------------------------------------------------- Fields end */
+	/**
+	 * 实现ApplicationContextAware接口, 注入Context到静态变量中.
+	 */
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		SpringContextUtil.applicationContext = applicationContext;
+	}
+
+	/**
+	 * @方法描述: 取得存储在静态变量中的ApplicationContext.
+	 * @return
+	 */
+	public static ApplicationContext getApplicationContext() {
+		assertContextInjected();
+		return applicationContext;
+	}
+
+	/**
+	 * @方法描述: 清除SpringContextHolder中的ApplicationContext为Null
+	 */
+	public static void clearHolder() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("清除SpringContextUtil中的ApplicationContext:" + applicationContext);
+		}
+	}
+
+	/**
+	 * @方法描述: 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+	 * @param name
+	 * @return
+	 * @throws BeansException
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T getBean(String name) throws BeansException {
+		try {
+			return (T) applicationContext.getBean(name);
+		} catch (Exception e) {
+			throw new RuntimeException("获取的Bean不存在！");
+		}
+	}
+
+	/**
+	 * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+	 */
+	public static <T> T getBean(Class<T> requiredType) {
+		assertContextInjected();
+		return applicationContext.getBean(requiredType);
+	}
+
+	/**
+	 * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+	 */
+	public static <T> T getBean(String name, Class<T> requiredType) throws BeansException {
+		return applicationContext.getBean(name, requiredType);
+	}
+
+	public static boolean containsBean(String name) {
+		return applicationContext.containsBean(name);
+	}
+
+	public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
+		return applicationContext.isSingleton(name);
+	}
+
+	public static Class<? extends Object> getType(String name)
+			throws NoSuchBeanDefinitionException {
+		return applicationContext.getType(name);
+	}
+
+	public static String[] getAliases(String name) throws NoSuchBeanDefinitionException {
+		return applicationContext.getAliases(name);
+	}
+
+	/**
+	 * 实现DisposableBean接口, 在Context关闭时清理静态变量.
+	 */
+	@Override
+	public void destroy() throws Exception {
+		SpringContextUtil.clearHolder();
+	}
+
+	/**
+	 * @方法描述: 检查ApplicationContext不为空
+	 */
+	private static void assertContextInjected() {
+		Validate.validState(applicationContext != null,
+				"applicaitonContext属性未注入, 请在applicationContext.xml中定义SpringContextUtil.");
+	}
+}
