299f9627913ced3f913160f776a960f5e54bdf24
皇族灬战狼 <duleilewuhen@163.com>
2017-11-11 00:58:57 +0800

    会员中心

---
+++ wechat-manage/src/main/java/com/cdeledu/controller/ucenter/MemberOperateController.java
@@ -0,0 +1,15 @@
+package com.cdeledu.controller.ucenter;
+
+import com.cdeledu.controller.BaseController;
+
+public class MemberOperateController extends BaseController {
+	private static final long serialVersionUID = 1L;
+	/** ----------------------------------------------------- Fields start */
+	/** ----------------------------------------------------- Fields end */
+
+	/** ----------------------------------------------- [私有方法] */
+	/** ----------------------------------------------- [私有方法] */
+
+	/** ----------------------------------------------- [测试方法] */
+	/** ----------------------------------------------- [测试方法] */
+}
