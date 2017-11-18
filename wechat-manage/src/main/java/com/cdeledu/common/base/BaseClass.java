93830ef1e0aa06aac2d8d72ed226e6512f01380e
皇族灬战狼 <duleilewuhen@163.com>
2017-11-18 10:39:39 +0800

    在基类中封装日志输出类,便于其所有集成类中，直接调用日志输出，不必在重复定义日志输出

--- wechat-manage/src/main/java/com/cdeledu/common/base/BaseClass.java
+++ wechat-manage/src/main/java/com/cdeledu/common/base/BaseClass.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.servlet.jsp.tagext.TagSupport;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,4 +21,107 @@ public abstract class BaseClass extends TagSupport implements Serializable {
 	 * 日志对象
 	 */
 	protected Logger logger = LoggerFactory.getLogger(getClass());
+
+	/**
+	 * @方法描述: Debug 输出
+	 * @param clazz
+	 *            目标.class
+	 * @param message
+	 *            输出信息
+	 */
+	public void debug(Class<? extends Object> clazz, String message) {
+		if (!logger.isDebugEnabled())
+			return;
+		Logger logger = LoggerFactory.getLogger(clazz);
+		logger.debug(message);
+	}
+
+	/**
+	 * @方法描述: Debug 输出
+	 * @param clazz
+	 *            目标.Class
+	 * @param fmtString
+	 *            输出信息key
+	 * @param value
+	 *            输出信息value
+	 */
+	public void fmtDebug(Class<? extends Object> clazz, String fmtString, Object... value) {
+		if (!logger.isDebugEnabled())
+			return;
+		if (StringUtils.isBlank(fmtString)) {
+			return;
+		}
+		if (null != value && value.length != 0) {
+			fmtString = String.format(fmtString, value);
+		}
+		debug(clazz, fmtString);
+	}
+
+	/**
+	 * @方法描述: Error 输出
+	 * @param clazz
+	 *            目标.class
+	 * @param message
+	 *            输出信息
+	 * @param e
+	 *            异常类
+	 */
+	public void error(Class<? extends Object> clazz, String message, Exception e) {
+		Logger logger = LoggerFactory.getLogger(clazz);
+		if (null == e) {
+			logger.error(message);
+			return;
+		}
+		logger.error(message, e);
+	}
+
+	/**
+	 * @方法描述: Error 输出
+	 * @param clazz
+	 *            目标.class
+	 * @param message
+	 *            输出信息
+	 */
+	public void error(Class<? extends Object> clazz, String message) {
+		error(clazz, message, null);
+	}
+
+	/**
+	 * @方法描述: Error 输出
+	 * @param fmtString
+	 *            输出信息key
+	 * @param e
+	 *            异常类
+	 * @param value
+	 *            输出信息value
+	 */
+	public void fmtError(Class<? extends Object> clazz, Exception e, String fmtString,
+			Object... value) {
+		if (StringUtils.isBlank(fmtString)) {
+			return;
+		}
+		if (null != value && value.length != 0) {
+			fmtString = String.format(fmtString, value);
+		}
+		error(clazz, fmtString, e);
+	}
+
+	/**
+	 * @方法描述: 异常填充值输出
+	 * @param clazz
+	 *            目标.Class
+	 * @param fmtString
+	 *            输出信息key
+	 * @param value
+	 *            输出信息value
+	 */
+	public void fmtError(Class<? extends Object> clazz, String fmtString, Object... value) {
+		if (StringUtils.isBlank(fmtString)) {
+			return;
+		}
+		if (null != value && value.length != 0) {
+			fmtString = String.format(fmtString, value);
+		}
+		error(clazz, fmtString);
+	}
 }
