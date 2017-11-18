045ae7621a1df75faa6da9dbdfc1c1c90b2e9752
皇族灬战狼 <duleilewuhen@163.com>
2017-11-18 10:57:44 +0800

    防止常见的xss攻击、 SQL注入

---
+++ wechat-manage/src/main/java/com/cdeledu/core/xss/XssHttpServletRequestWrapper.java
@@ -0,0 +1,104 @@
+package com.cdeledu.core.xss;
+
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 防止常见的XSS和SQL注入
+ * @创建者: 皇族灬战狼
+ * @创建时间: 2017年11月18日 上午10:55:01
+ * @版本: V1.0
+ * @since: JDK 1.7
+ * @see <a href="http://blog.csdn.net/hithedy/article/details/50630109">
+ *      防止常见XSS 过滤 SQL注入</a>
+ */
+public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+	public XssHttpServletRequestWrapper(HttpServletRequest request) {
+		super(request);
+	}
+
+	@Override
+	public String[] getParameterValues(String parameter) {
+		String[] values = super.getParameterValues(parameter);
+		if (values == null) {
+			return null;
+		}
+		int count = values.length;
+		String[] encodedValues = new String[count];
+		for (int i = 0; i < count; i++) {
+			encodedValues[i] = stripXSS(values[i]);
+		}
+		return encodedValues;
+	}
+
+	/**
+	 * 覆盖getParameter方法，将参数名和参数值都做xss & sql过滤。<br/>
+	 * 如果需要获得原始的值，则通过super.getParameterValues(name)来获取<br/>
+	 */
+	@Override
+	public String getParameter(String parameter) {
+		String value = super.getParameter(parameter);
+		if (StringUtils.isNotBlank(value)) {
+			return stripXSS(value);
+		}
+		return "";
+	}
+
+	/**
+	 * 覆盖getHeader方法，将参数名和参数值都做xss & sql过滤。<br/>
+	 * 如果需要获得原始的值，则通过super.getHeaders(name)来获取<br/>
+	 */
+	@Override
+	public String getHeader(String name) {
+		String value = super.getHeader(name);
+		return stripXSS(value);
+	}
+
+	/**
+	 * @方法描述: 防止xss跨脚本攻击（替换，根据实际情况调整）
+	 * @param value
+	 * @return
+	 */
+	private String stripXSS(String value) {
+		if (value != null) {
+
+			value = value.replaceAll("", "");
+			Pattern scriptPattern = Pattern.compile("(.*?)", Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+			scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+			scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+			scriptPattern = Pattern.compile("", Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+			scriptPattern = Pattern.compile("",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+			scriptPattern = Pattern.compile("eval\\((.*?)\\)",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+			scriptPattern = Pattern.compile("e­xpression\\((.*?)\\)",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+			scriptPattern = Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+			scriptPattern = Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+			scriptPattern = Pattern.compile("onload(.*?)=",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+		}
+		return value;
+	}
+}
