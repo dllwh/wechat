ad404739b2f6d8219245002e5d00e653fa03019e
皇族灬战狼 <duleilewuhen@163.com>
2017-12-03 16:24:01 +0800

    与缓存相关的常量

---
+++ wechat-manage/src/main/java/com/cdeledu/common/constants/CacheConstans.java
@@ -0,0 +1,19 @@
+package com.cdeledu.common.constants;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 与缓存相关的常量
+ * @创建者: 皇族灬战狼
+ * @创建时间: 2017年11月29日 下午12:02:40
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public final class CacheConstans {
+	/** shiro 缓存的前缀 */
+	public static final String redis_shiro_cache = "shiro-cache:";
+	/** Redis session Key 的前缀 */
+	public static final String redis_shiro_session = "shiro-session:";
+}
