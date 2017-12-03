81771e1464bc60195f3dc01dcb3f964cbf878a52
皇族灬战狼 <duleilewuhen@163.com>
2017-12-03 16:23:35 +0800

    会话管理与调度管理

---
+++ wechat-manage/src/main/java/com/cdeledu/controller/system/monitor/ScheduledController.java
@@ -0,0 +1,31 @@
+package com.cdeledu.controller.system.monitor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cdeledu.controller.BaseController;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 调度管理:主要针对的是定时任务
+ * @创建者: 皇族灬战狼
+ * @创建时间: 2017年12月1日 下午5:08:45
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+@Controller
+@RequestMapping("scheduled")
+public class ScheduledController extends BaseController {
+	/** ----------------------------------------------------- Fields start */
+	private static final long serialVersionUID = 1L;
+	/** ----------------------------------------------------- Fields end */
+
+	/** ----------------------------------------------- [私有方法] */
+	/** ----------------------------------------------- [私有方法] */
+
+	/** ----------------------------------------------- [测试方法] */
+	/** ----------------------------------------------- [测试方法] */
+}
---
+++ wechat-manage/src/main/java/com/cdeledu/controller/system/monitor/SysSessionController.java
@@ -0,0 +1,31 @@
+package com.cdeledu.controller.system.monitor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cdeledu.controller.BaseController;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 会话管理
+ * @创建者: 皇族灬战狼
+ * @创建时间: 2017年12月1日 下午5:07:21
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+@Controller
+@RequestMapping("sysSession")
+public class SysSessionController extends BaseController {
+	/** ----------------------------------------------------- Fields start */
+	private static final long serialVersionUID = 1L;
+	/** ----------------------------------------------------- Fields end */
+
+	/** ----------------------------------------------- [私有方法] */
+	/** ----------------------------------------------- [私有方法] */
+
+	/** ----------------------------------------------- [测试方法] */
+	/** ----------------------------------------------- [测试方法] */
+}
