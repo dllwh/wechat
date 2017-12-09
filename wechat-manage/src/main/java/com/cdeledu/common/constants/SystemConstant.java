950f6d544863705f3ccde8e1a04ba8f7cd867688
皇族灬战狼 <duleilewuhen@163.com>
2017-12-09 16:04:10 +0800

    系统级常量

--- wechat-manage/src/main/java/com/cdeledu/common/constants/SystemConstant.java
+++ wechat-manage/src/main/java/com/cdeledu/common/constants/SystemConstant.java
@@ -13,35 +13,17 @@ package com.cdeledu.common.constants;
  * @since: JDK 1.7
  */
 public class SystemConstant {
-	/**
-	 * 
-	 * 把今天最好的表现当作明天最新的起点．．～
-	 *
-	 * Today the best performance as tomorrow newest starter!
-	 *
-	 * @类描述: 菜单类型
-	 * @创建者: 皇族灬战狼
-	 * @创建时间: 2017年11月8日 上午11:15:05
-	 * @版本: V1.0
-	 * @since: JDK 1.7
-	 */
+	/** 菜单类型 */
 	public enum SysMenuType {
-		/**
-		 * 目录
-		 */
+		/** 目录 */
 		CATALOG(0),
-		/**
-		 * 菜单
-		 */
+		/** 菜单 */
 		MENU(1),
-		/**
-		 * 按钮
-		 */
+		/** 按钮 */
 		BUTTON(2);
+		int value;
 
-		private int value;
-
-		private SysMenuType(int value) {
+		SysMenuType(int value) {
 			this.value = value;
 		}
 
@@ -50,31 +32,15 @@ public class SystemConstant {
 		}
 	}
 
-	/**
-	 * 
-	 * 把今天最好的表现当作明天最新的起点．．～
-	 *
-	 * Today the best performance as tomorrow newest starter!
-	 *
-	 * @类描述: 定时任务状态
-	 * @创建者: 皇族灬战狼
-	 * @创建时间: 2017年11月8日 上午11:15:27
-	 * @版本: V1.0
-	 * @since: JDK 1.7
-	 */
+	/** 定时任务状态 */
 	public enum SysScheduleStatus {
-		/**
-		 * 正常
-		 */
+		/** 正常 */
 		NORMAL(1),
-		/**
-		 * 暂停
-		 */
+		/** 暂停 */
 		PAUSE(0);
+		int value;
 
-		private int value;
-
-		private SysScheduleStatus(int value) {
+		SysScheduleStatus(int value) {
 			this.value = value;
 		}
 
@@ -83,43 +49,20 @@ public class SystemConstant {
 		}
 	}
 
-	/**
-	 * 
-	 * 把今天最好的表现当作明天最新的起点．．～
-	 *
-	 * Today the best performance as tomorrow newest starter!
-	 *
-	 * @类描述: 系统通用状态
-	 * @创建者: 皇族灬战狼
-	 * @创建时间: 2017年11月8日 上午11:16:19
-	 * @版本: V1.0
-	 * @since: JDK 1.7
-	 */
+	/** 系统通用状态 */
 	public enum SysStatusType {
-
-		/**
-		 * 禁用，隐藏
-		 */
+		/** 禁用，隐藏 */
 		DISABLE(0),
-
-		/**
-		 * 可用，显示
-		 */
+		/** 可用，显示 */
 		ENABLE(1),
-
-		/**
-		 * 显示
-		 */
+		/** 显示 */
 		SHOW(1),
-
-		/**
-		 * 隐藏
-		 */
+		/** 隐藏 */
 		HIDDEN(0);
 
-		private int value;
+		int value;
 
-		private SysStatusType(int value) {
+		SysStatusType(int value) {
 			this.value = value;
 		}
 
@@ -128,31 +71,35 @@ public class SystemConstant {
 		}
 	}
 
-	/**
-	 * 
-	 * 把今天最好的表现当作明天最新的起点．．～
-	 *
-	 * Today the best performance as tomorrow newest starter!
-	 *
-	 * @类描述: 日志类型
-	 * @创建者: 皇族灬战狼
-	 * @创建时间: 2017年11月9日 下午5:33:11
-	 * @版本: V1.0
-	 * @since: JDK 1.7
-	 */
+	/** 日志类型 */
 	public enum SysOpType {
-		LOGIN("login"), // 登录
-		EXIT("exit"), // 退出
-		INSERT("insert"), // 插入
-		DEL("delete"), // 删除
-		UPDATE("update"), // 更新
-		UPLOAD("upload"), // 上传
-		SELECT("select"), // 选择、查询
-		DOWNLOAD("download"), // 下载
-		OTHER("other");// 其他
-		private String value;
+		/** 登录日志 */
+		LOGIN("登录日志"),
+		/** 登录失败日志 */
+		LOGIN_FAIL("登录失败日志"),
+		/** 退出日志 */
+		EXIT("退出日志"),
+		/** 异常日志 */
+		EXCEPTION("异常日志"),
+		/** 业务日志 */
+		BUSSINESS("业务日志"),
+		/** 插入 */
+		INSERT("insert"),
+		/** 删除 */
+		DEL("delete"),
+		/** 更新 */
+		UPDATE("update"),
+		/** 上传 */
+		UPLOAD("upload"),
+		/** 选择、查询 */
+		SELECT("select"),
+		/** 下载 */
+		DOWNLOAD("download"),
+		/** 其他 */
+		OTHER("other");
+		String value;
 
-		private SysOpType(String value) {
+		SysOpType(String value) {
 			this.value = value;
 		}
 
@@ -161,18 +108,9 @@ public class SystemConstant {
 		}
 	}
 
-	/**
-	 * 把今天最好的表现当作明天最新的起点．．～
-	 *
-	 * Today the best performance as tomorrow newest starter!
-	 *
-	 * @类描述: 日志级别
-	 * @创建者: 皇族灬战狼
-	 * @创建时间: 2017年12月8日 上午11:31:48
-	 * @版本: V1.0
-	 * @since: JDK 1.7
-	 */
+	/** 日志级别 */
 	public enum SysLogLeavel {
 		info, warn, error;
 	}
+
 }
