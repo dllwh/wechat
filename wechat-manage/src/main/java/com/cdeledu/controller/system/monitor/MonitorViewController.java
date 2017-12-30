261dc3f53f786bf8844438ece8279cad1c3f682e
皇族灬战狼 <duleilewuhen@163.com>
2017-12-30 16:18:33 +0800

    监控系统管理

--- wechat-manage/src/main/java/com/cdeledu/controller/system/monitor/MonitorViewController.java
+++ wechat-manage/src/main/java/com/cdeledu/controller/system/monitor/MonitorViewController.java
@@ -5,14 +5,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cdeledu.controller.BaseController;
 
 /**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
  * @类描述: 监控系统管理
  * @创建者: 皇族灬战狼
+ * @联系方式: duleilewuhen@sina.com
  * @创建时间: 2017年3月5日 下午5:07:29
  * @版本: V1.0
  * @since: JDK 1.7
@@ -21,17 +25,30 @@ import com.cdeledu.controller.BaseController;
 @RequestMapping("monitor")
 public class MonitorViewController extends BaseController {
 	private static final long serialVersionUID = 1L;
+
 	/** ----------------------------------------------------- Fields start */
 	/** ----------------------------------------------------- Fields end */
 
-	@RequestMapping(value = "server/info")
-	@ResponseBody
+		/**
+		 * @方法描述 : 
+		 * @param request
+		 * @param response
+		 * @return
+		 */
+	@RequestMapping(value = "serverInfo")
 	public ModelAndView serverInfo(HttpServletRequest request, HttpServletResponse response) {
 		ModelAndView mv = this.getModelAndView();
-		mv.setViewName("/system/info");
+		mv.setViewName("system/monitor/server/serverInfo");
 		return mv;
 	}
-	
+
+	@RequestMapping(value = "cache")
+	public ModelAndView cacheInfo(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = this.getModelAndView();
+		mv.setViewName("system/monitor/cache/cacheInit");
+		return mv;
+	}
+
 	/** ----------------------------------------------- [私有方法] */
 	/** ----------------------------------------------- [私有方法] */
 }
