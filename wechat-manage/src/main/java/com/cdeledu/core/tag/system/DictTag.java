f37f8cfeef2be7ba8f0585c4bdaab18798d306e7
独泪了无痕 <duleilewuhen@sina.com>
2017-12-31 20:48:40 +0800

    增加

--- wechat-manage/src/main/java/com/cdeledu/core/tag/system/DictTag.java
+++ wechat-manage/src/main/java/com/cdeledu/core/tag/system/DictTag.java
@@ -1,28 +1,20 @@
 package com.cdeledu.core.tag.system;
 
-import javax.servlet.jsp.JspException;
-
 import com.cdeledu.common.base.BaseClass;
 
 /**
- * 
  * 把今天最好的表现当作明天最新的起点．．～
  *
  * Today the best performance as tomorrow newest starter!
  *
- * @类描述: 用与停止页面继续加载的标签
- * @创建者: 皇族灬战狼
- * @创建时间: 2017年12月8日 下午3:36:24
+ * @类描述: 数据字典
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2017年12月31日 下午8:46:22
  * @版本: V1.0
- * @since: JDK 1.7	
+ * @since: JDK 1.7
  */
-public class StopPage extends BaseClass {
+public class DictTag extends BaseClass {
 	/** ----------------------------------------------------- Fields start */
 	private static final long serialVersionUID = 1L;
-
 	/** ----------------------------------------------------- Fields end */
-
-	public int doEndTag() throws JspException {
-		return SKIP_PAGE;// 让服务器不要处理剩余的页面
-	}
 }
