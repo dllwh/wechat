748221e1738adfe8707627696062ba344c4707b4
皇族灬战狼 <duleilewuhen@163.com>
2018-01-17 15:09:05 +0800

    Bootstrap Fileinput文件上传组件

--- wechat-manage/src/main/webapp/plug-in/tools/bootstrapExt.js
+++ wechat-manage/src/main/webapp/plug-in/tools/bootstrapExt.js
@@ -44,6 +44,40 @@ $.fn.bootstrapTableEx = function(opt) {
 }
 
 
+$.fn.fileinputExt = function(opt) {
+	var defaults = {
+		uploadUrl : "",//上传的地址
+		language : "zh", //设置语言
+		uploadAsync : true, //异步上传
+		showCaption : true, //是否显示标题
+		showUpload : true, //是否显示上传按钮
+		showRemove : true, //是否显示移除按钮
+		showPreview : true, //是否显示预览按钮
+		showCancel:true,//是否显示取消按钮
+		showClose:true,//是否显示关闭按钮
+		browseClass : "btn btn-primary", //按钮样式 
+		dropZoneEnabled : false, //是否显示拖拽区域
+		minImageWidth: null, //图片的最小宽度
+		minImageHeight: null,//图片的最小高度
+		maxImageWidth: null,//图片的最大宽度
+		maxImageHeight: null,//图片的最大高度
+		maxFileSize : 20480, //单位为kb，如果为0表示不限制文件大小
+		minFileSize : 0,// 单位为kb，上传文件的最小大小值
+		allowedPreviewTypes : null,
+		// allowedFileTypes: ['image','html','text','video','audio','flash','object'],//文件允许的类型
+		allowedFileTypes: null,
+		// allowedFileExtensions : [ "xls", "xlsx", "ppt", "pptx", "doc", "docx" ], //接收的文件后缀
+		allowedFileExtensions : null, //接收的文件后缀
+		maxFileCount : 10, // 表示允许同时上传的最大文件个数
+		enctype: 'multipart/form-data',
+		validateInitialCount:true,
+		previewFileIcon : '<i class="glyphicon glyphicon-file"></i>',
+		msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}！"
+	}
+	var option = $.extend({}, defaults, opt);
+	$(this).fileinput(option);
+}
+
 /**
  * 获取查询的参数 
  */
