45136c3c0f2f1664ffe0f32d7edc079a0739db1b
皇族灬战狼 <duleilewuhen@163.com>
2018-01-31 14:02:06 +0800

    分页扩展字段

--- wechat-common/src/main/java/com/cdeledu/common/base/PageEntity.java
+++ wechat-common/src/main/java/com/cdeledu/common/base/PageEntity.java
@@ -15,15 +15,19 @@ import org.apache.commons.lang3.StringUtils;
 public class PageEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 	/** 当前页数:第几页 --pageNo */
-	protected int page = 1;
+	protected Integer page = 1;
+	protected Integer pageNumber = 1;
 	/** 每页记录数 :pageSize(设置为“-1”表示不进行分页（分页无效）) */
-	protected int rows = 10;
+	protected Integer rows = 10;
+	protected Integer pageSize = 10;
 	/** 起始页 */
 	protected int startRow;
 	/** 排序字段名 */
 	protected String sort;
+	protected String sortName;
 	/** 按什么排序(asc,desc) */
 	protected String order;
+	protected String sortOrder;
 
 	public boolean hasField(String fieldName) {
 		if (null == fieldName) {
@@ -47,12 +51,28 @@ public class PageEntity implements Serializable {
 		this.page = page;
 	}
 
+	public Integer getPageNumber() {
+		return pageNumber;
+	}
+
+	public void setPageNumber(Integer pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
 	public int getRows() {
 		return rows;
 	}
 
 	public void setRows(int rows) {
 		this.rows = rows;
+	}
+
+	public Integer getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
 	}
 
 	public int getStartRow() {
@@ -72,17 +92,43 @@ public class PageEntity implements Serializable {
 		}
 	}
 
+	public String getSortName() {
+		return sortName;
+	}
+
+	public void setSortName(String sortName) {
+		if (hasField(sortName)) {
+			this.sortName = sortName;
+		} else {
+			this.sort = "";
+		}
+	}
+
 	public String getOrder() {
 		return order;
 	}
 
 	public void setOrder(String order) {
-		if(StringUtils.isNoneBlank(order) && (order.equalsIgnoreCase("asc") || order.equalsIgnoreCase("desc"))){
+		if (StringUtils.isNoneBlank(order)
+				&& (order.equalsIgnoreCase("asc") || order.equalsIgnoreCase("desc"))) {
 			this.order = order;
 		} else {
 			this.order = "";
 		}
-		
+
+	}
+
+	public String getSortOrder() {
+		return sortOrder;
+	}
+
+	public void setSortOrder(String sortOrder) {
+		if (StringUtils.isNoneBlank(sortOrder)
+				&& (sortOrder.equalsIgnoreCase("asc") || sortOrder.equalsIgnoreCase("desc"))) {
+			this.sortOrder = sortOrder;
+		} else {
+			this.sortOrder = "";
+		}
 	}
 
 }
