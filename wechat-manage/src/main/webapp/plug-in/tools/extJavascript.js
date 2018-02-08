947fece758acce405d7fb6d185ca1c635649ae79
皇族灬战狼 <duleilewuhen@163.com>
2018-02-08 11:51:22 +0800

    页面全屏相关操作

--- wechat-manage/src/main/webapp/plug-in/tools/extJavascript.js
+++ wechat-manage/src/main/webapp/plug-in/tools/extJavascript.js
@@ -178,3 +178,27 @@ dllwh.isExitsVariable = function (variableName){
 	}
 	return false;
 }
+
+// 进入全屏
+dllwh.requestFullScreen = function() {
+	var de = document.documentElement;
+	if (de.requestFullscreen) {
+		de.requestFullscreen();
+	} else if (de.mozRequestFullScreen) {
+		de.mozRequestFullScreen();
+	} else if (de.webkitRequestFullScreen) {
+		de.webkitRequestFullScreen();
+	}
+}
+
+// 退出全屏
+dllwh.exitFullscreen = function () {
+	var de = document;
+	if (de.exitFullscreen) {
+		de.exitFullscreen();
+	} else if (de.mozCancelFullScreen) {
+		de.mozCancelFullScreen();
+	} else if (de.webkitCancelFullScreen) {
+		de.webkitCancelFullScreen();
+	}
+}
