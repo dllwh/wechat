bbdfe531d60b2e1c8b63ce10990212ba5f7bd89f
独泪了无痕 <duleilewuhen@sina.com>
2018-02-25 21:52:31 +0800

    bootstrap table分页效果

--- wechat-manage/src/main/webapp/plug-in/tools/bootstrapExt.js
+++ wechat-manage/src/main/webapp/plug-in/tools/bootstrapExt.js
@@ -4,20 +4,19 @@
 $.fn.bootstrapTableEx = function(opt) {
 	var defaults = {
 		toolbar:'',
-		url : '',
+		url : '', 							// 请求后台的URL（*）
 		dataField : "rows",
-		method : 'post',
 		dataType : 'json',
 		selectItemName : 'id',
 		smartDisplay : false,
-		method: 'get',                      // 请求方式（*）
+		method: 'post',                     // 请求方式（*）
 		toolbar: '',       					// 工具按钮用哪个容器
 		iconsPrefix:'glyphicon',            // 定义字体库 ('Glyphicon' or 'fa' for FontAwesome)
 		striped: true,                      // 是否显示行间隔色
 		cache: false,                       // 是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
-		sortable: false,                    // 是否启用排序
+		sortable: true,                     // 是否启用排序
 		queryParamsType: '',
-		// queryParams: queryParams,			// 传递参数（*）
+		queryParams: queryParams,			// 传递参数（*）
 		sidePagination: "server",           // 分页方式：client客户端分页，server服务端分页（*）
 		pageNumber: 1,                      // 初始化加载第一页，默认第一页
 		pageSize: 10,                       // 每页的记录行数（*）
@@ -27,20 +26,54 @@ $.fn.bootstrapTableEx = function(opt) {
 		showFooter:false,					// 是否显示列脚
 		showColumns: true,                  // 是否显示所有的列
 		showRefresh: true,                  // 是否显示刷新按钮
+		showToggle: false,                  // 是否显示详细视图和列表视图的切换按钮
 		minimumCountColumns: 2,             // 最少允许的列数
 		clickToSelect: true,                // 是否启用点击选中行
 		cardView: false,                    // 是否显示详细视图
 		detailView: false,                  // 是否显示父子表
 		pagination: true,                   // 是否显示分页（*）
+		paginationLoop:false,				// 设置为 true 启用分页条无限循环的功能
+		onlyInfoPagination:false,			// 设置为 true 只显示总数据数，而不显示分页按钮
 		paginationPreText:"上一页",			// 指定分页条中上一页按钮的图标或文字
 		paginationNextText:"下一页",			// 指定分页条中下一页按钮的图标或文字
 		clickToSelect:true,					// 设置true 将在点击行时，自动选择rediobox 和 checkbox
 		singleSelect:true,					// 设置True 将禁止多选
 		maintainSelected:false,				// 设置为 true 在点击分页按钮或搜索按钮时，将记住checkbox的选择项
-		columns : []
+		contentType: "application/x-www-form-urlencoded",
+		columns : [],
+		formatDetailPagination : function(totalRows) {
+			return "总共显示 " + totalRows + " 条记录";
+		},
+		onLoadSuccess: function(data){  // 加载成功时执行  
+			if(dllwh.isNotNullOrEmpty(data.success)&& !data.success){
+				dialogMsg(data.msg, "error");
+			}
+		},
+		onLoadError: function(status){  // 加载失败时执行
+			dialogMsg("数据加载失败，请重新刷新页面", "error");
+		},
+		formatShowingRows : function(pageFrom, pageTo, totalRows) {
+			if(totalRows >0){
+				return "显示第 " + pageFrom + " 到第 " + pageTo + " 条记录，总共 " + totalRows + " 条记录"
+			} else {
+				return "";
+			}
+			
+		},
+		formatNoMatches: function () {//没有匹配的结果
+			$(".page-next").addClass('disabled');
+			return '无符合条件的记录';
+		}
 	}
 	var option = $.extend({}, defaults, opt);
 	$(this).bootstrapTable(option);
+}
+
+/**
+ * 获取查询的参数 
+ */
+function queryParams(params) {
+	return {};
 }
 
 /**
