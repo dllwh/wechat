40079c02fdb0f603c91d5ecb6646c98da44f521d
独泪了无痕 <duleilewuhen@sina.com>
2018-03-02 22:43:54 +0800

    通知公告表

---
+++ wechat-manage/src/main/java/com/cdeledu/model/system/SysSession.java
@@ -0,0 +1,10 @@
+package com.cdeledu.model.system;
+
+import com.cdeledu.common.base.BaseEntity;
+
+public class SysSession extends BaseEntity {
+	/** ----------------------------------------------------- Fields start */
+	private static final long serialVersionUID = 1L;
+
+	/** ----------------------------------------------------- Fields end */
+}
