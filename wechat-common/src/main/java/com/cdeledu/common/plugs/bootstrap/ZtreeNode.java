2cc8e83d974d0db6871c987a2ac9c6a31d9c0dcb
独泪了无痕 <duleilewuhen@sina.com>
2018-03-18 23:50:41 +0800

    跳转

--- wechat-common/src/main/java/com/cdeledu/common/plugs/bootstrap/ZtreeNode.java
+++ wechat-common/src/main/java/com/cdeledu/common/plugs/bootstrap/ZtreeNode.java
@@ -3,8 +3,6 @@ package com.cdeledu.common.plugs.bootstrap;
 import java.io.Serializable;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 /**
  * 把今天最好的表现当作明天最新的起点．．～
  *
@@ -20,6 +18,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public class ZtreeNode implements Serializable {
 	private static final long serialVersionUID = 1L;
+	/** 节点的标识属性 */
+	private Integer id;
+	/** 节点parentId属性，命名规则同id */
+	private Integer pId;
 	/** 节点显示的文本 */
 	private String name;
 	/** 节点是否勾选 */
@@ -37,127 +39,88 @@ public class ZtreeNode implements Serializable {
 	/** 节点折叠时的图标 */
 	private String iconClose;
 	/** 判断 treeNode 节点是否被隐藏 ;true 表示被隐藏;false 表示被显示 */
-	private String isHidden = "false";
+	private boolean isHidden = false;
 	/** 是否为父节点 */
-	private String IsParent = "false";
-	/** 节点的标识属性 */
-	private Integer id;
-	/** 节点parentId属性，命名规则同id */
-	private Integer pId;
+	private boolean IsParent = false;
 	/** 得到该节点所有孩子节点，直接下级，若要得到所有下属层级节点，需要自己写递归得到 */
 	private List<ZtreeNode> children;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public boolean isChecked() {
-		return checked;
-	}
-
-	public void setChecked(boolean checked) {
-		this.checked = checked;
-	}
-
-	public boolean isChkDisabled() {
-		return chkDisabled;
-	}
-
-	public void setChkDisabled(boolean chkDisabled) {
-		this.chkDisabled = chkDisabled;
-	}
-
-	public boolean isNocheck() {
-		return nocheck;
-	}
-
-	public void setNocheck(boolean nocheck) {
-		this.nocheck = nocheck;
-	}
-
-	public boolean isOpen() {
-		return open;
-	}
-
-	public void setOpen(boolean open) {
-		this.open = open;
-	}
-
-	public String getIcon() {
-		return icon;
-	}
-
-	public void setIcon(String icon) {
-		this.icon = icon;
-	}
-
-	public String getIconOpen() {
-		return iconOpen;
-	}
-
-	public void setIconOpen(String iconOpen) {
-		this.iconOpen = iconOpen;
-	}
-
-	public String getIconClose() {
-		return iconClose;
-	}
-
-	public void setIconClose(String iconClose) {
-		this.iconClose = iconClose;
-	}
-
-	@JsonProperty(value = "isHidden")  
-	public String isHidden() {
-		return isHidden;
-	}
-
-	public void setHidden(String isHidden) {
-		this.isHidden = isHidden;
-	}
-
-	@JsonProperty(value = "isParent")  
-	public String isParent() {
-		return IsParent ;
-	}
-
-	public void setParent(String isParent) {
-		this.IsParent  = isParent;
-	}
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public Integer getpId() {
 		return pId;
 	}
-
 	public void setpId(Integer pId) {
 		this.pId = pId;
 	}
-
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isChecked() {
+		return checked;
+	}
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+	public boolean isChkDisabled() {
+		return chkDisabled;
+	}
+	public void setChkDisabled(boolean chkDisabled) {
+		this.chkDisabled = chkDisabled;
+	}
+	public boolean isNocheck() {
+		return nocheck;
+	}
+	public void setNocheck(boolean nocheck) {
+		this.nocheck = nocheck;
+	}
+	public boolean isOpen() {
+		return open;
+	}
+	public void setOpen(boolean open) {
+		this.open = open;
+	}
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	public String getIconOpen() {
+		return iconOpen;
+	}
+	public void setIconOpen(String iconOpen) {
+		this.iconOpen = iconOpen;
+	}
+	public String getIconClose() {
+		return iconClose;
+	}
+	public void setIconClose(String iconClose) {
+		this.iconClose = iconClose;
+	}
+	public boolean getIsHidden() {
+		return isHidden;
+	}
+	public void setIsHidden(boolean isHidden) {
+		this.isHidden = isHidden;
+	}
+	public boolean getIsParent() {
+		return IsParent;
+	}
+	public void setIsParent(boolean isParent) {
+		IsParent = isParent;
+	}
 	public List<ZtreeNode> getChildren() {
 		return children;
 	}
-
 	public void setChildren(List<ZtreeNode> children) {
 		this.children = children;
 	}
-
-	@Override
-	public String toString() {
-		return "ZtreeNode [name=" + name + ", checked=" + checked + ", chkDisabled=" + chkDisabled
-				+ ", nocheck=" + nocheck + ", open=" + open + ", icon=" + icon + ", iconOpen="
-				+ iconOpen + ", iconClose=" + iconClose + ", isHidden=" + isHidden + ", isParent="
-				+ IsParent  + ", id=" + id + ", pId=" + pId + ", children=" + children + "]";
-	}
+	
 }
