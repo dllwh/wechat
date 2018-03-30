4e2bd46de171cca2990c07e28f9c4a04212589d1
独泪了无痕 <duleilewuhen@sina.com>
2018-03-30 16:50:27 +0800

    Java 二维码的生成与解析示例代码

--- wechat-utils/src/main/java/com/cdeledu/util/apache/io/QrcodeUtilHelper.java
+++ wechat-utils/src/main/java/com/cdeledu/util/apache/io/QrcodeUtilHelper.java
@@ -3,6 +3,8 @@ package com.cdeledu.util.apache.io;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Hashtable;
 
 import javax.imageio.ImageIO;
@@ -12,6 +14,7 @@ import org.apache.commons.lang3.StringUtils;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.DecodeHintType;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.MultiFormatWriter;
@@ -21,6 +24,7 @@ import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 /**
  * @类描述: 利用zxing开源工具生成二维码QRCode与解析通用类
@@ -43,20 +47,27 @@ public class QrcodeUtilHelper {
 	 * @param height
 	 *            高度
 	 */
-	@SuppressWarnings("deprecation")
-	public static void encode(String content, String imgPath, int width,int height) {
+	@SuppressWarnings("rawtypes")
+	public static <T> void encode(String content, String imgPath, int width, int height) {
 		if (StringUtils.isBlank(content) || StringUtils.isBlank(imgPath)) {
 			return;
 		}
 		BitMatrix byteMatrix;
+		// 定义二维码内容参数
+		HashMap<EncodeHintType, Comparable> hints = new HashMap<EncodeHintType, Comparable>();
+		// 设置字符集编码格式
+		hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+		// 设置容错等级，在这里我们使用M级别
+		hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+		// 设置边框距
+		hints.put(EncodeHintType.MARGIN, 2);
 		try {
-			 //消除乱码
-			content = new String(content.getBytes("utf-8"), "iso-8859-1");
 			// 生成矩阵
-			byteMatrix = new MultiFormatWriter().encode(content,BarcodeFormat.QR_CODE, width, height);
-			File file = new File(imgPath);
+			byteMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, width,
+					height);
+			Path file = new File(imgPath).toPath();
 			// 输出图像
-			MatrixToImageWriter.writeToFile(byteMatrix, "png", file);
+			MatrixToImageWriter.writeToPath(byteMatrix, "png", file);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -82,10 +93,10 @@ public class QrcodeUtilHelper {
 			LuminanceSource source = new BufferedImageLuminanceSource(image);
 			BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
 			Hashtable<DecodeHintType, Object> hints = new Hashtable<DecodeHintType, Object>();
-			//解码设置编码方式为：GBK
+			// 解码设置编码方式为：GBK
 			hints.put(DecodeHintType.CHARACTER_SET, "GBK");
 			result = new MultiFormatReader().decode(bitmap, hints);
-			// 对图像进行解码  
+			// 对图像进行解码
 			String resultStr = result.getText();
 			return resultStr;
 		} catch (IOException ioe) {
