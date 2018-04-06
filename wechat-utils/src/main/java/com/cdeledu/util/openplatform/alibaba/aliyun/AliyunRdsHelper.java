7d49bcbd18c5bed60f6499895dddad4426a76985
独泪了无痕 <duleilewuhen@sina.com>
2018-04-06 22:19:26 +0800

    查找指定实例、指定数据库的账号列表信息或某个指定账号的信息

--- wechat-utils/src/main/java/com/cdeledu/util/openplatform/alibaba/aliyun/AliyunRdsHelper.java
+++ wechat-utils/src/main/java/com/cdeledu/util/openplatform/alibaba/aliyun/AliyunRdsHelper.java
@@ -1,5 +1,13 @@
 package com.cdeledu.util.openplatform.alibaba.aliyun;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.collect.Maps;
+
 /**
  * 把今天最好的表现当作明天最新的起点．．～
  *
@@ -15,15 +23,42 @@ package com.cdeledu.util.openplatform.alibaba.aliyun;
 public final class AliyunRdsHelper {
 	/** ----------------------------------------------------- Fields start */
 	/** RDS API的服务接入地址 */
-	private static final String RDS_API = "https://rds.aliyuncs.com/";
+	private static final String RDS_API = "https://rds.aliyuncs.com/?";
 	/** API 版本号 */
 	private static final String apiVersion = "2014-08-15";
 	/** 用户的访问服务所用的密钥,用于标识访问者的身份 */
 	private String accessKeyId;
+	/** 用于加密签名、验证签名字符串的密钥 */
+	private String accessKeySecret;
+	private SecurityTokenHelper accessSecretHelper = null;
 
 	/** ----------------------------------------------------- Fields end */
-	public AliyunRdsHelper(String accessKeyId) {
+	public AliyunRdsHelper(String accessKeyId, String accessKeySecret) {
 		this.accessKeyId = accessKeyId;
+		this.accessKeySecret = accessKeySecret;
 	}
 
+	/**
+	 * @方法:查找指定实例、指定数据库的账号列表信息或某个指定账号的信息
+	 * @创建人:独泪了无痕
+	 * @param dBInstanceId
+	 *            实例的名称,不能为空
+	 * @param accountName
+	 *            账号的名称,能为空
+	 * @param ownerAccount
+	 *            用户的主账号,能为空
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidKeyException
+	 */
+	public String describeAccounts(String dBInstanceId, String accountName, String ownerAccount)
+			throws InvalidKeyException, NoSuchAlgorithmException {
+		accessSecretHelper = new SecurityTokenHelper(accessKeyId, accessKeySecret);
+		Map<String, String> paramsMap = Maps.newHashMap();
+		if (StringUtils.isBlank(dBInstanceId)) {
+			return "";
+		}
+		paramsMap.put("Action", "DescribeAccounts");
+		paramsMap.put("DBInstanceId", dBInstanceId);
+		return RDS_API + accessSecretHelper.createSecurityToken(paramsMap, "GET", apiVersion);
+	}
 }
