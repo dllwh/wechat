918e68dfa42e8821576c264a52934af8e7d1a505
独泪了无痕 <duleilewuhen@sina.com>
2018-04-20 19:11:14 +0800

    百度AI

---
+++ wechat-utils/src/main/java/com/cdeledu/util/openplatform/baidu/baiduAI/BaiduAiBaseHelper.java
@@ -0,0 +1,7 @@
+package com.cdeledu.util.openplatform.baidu.baiduAI;
+
+public class BaiduAiBaseHelper {
+	/** ----------------------------------------------------- Fields start */
+
+	/** ----------------------------------------------------- Fields end */
+}
