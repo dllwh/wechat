d491d08d74554556e726151e08c8150024dba0e1
独泪了无痕 <duleilewuhen@sina.com>
2018-05-31 22:03:15 +0800

    高德开放平台

---
+++ wechat-utils/src/main/java/com/cdeledu/util/openplatform/alibaba/amap/AmapHelper.java
@@ -0,0 +1,18 @@
+package com.cdeledu.util.openplatform.alibaba.amap;
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 高德开放平台
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年5月21日 下午11:30:26
+ * @版本: V1.0
+ * @since: JDK 1.7
+ * @see <a href="lbs.amap.com/">高德开放平台</a>
+ */
+public final class AmapHelper {
+	/** ----------------------------------------------------- Fields start */
+
+	/** ----------------------------------------------------- Fields end */
+}
