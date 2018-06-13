ed4f05aadbaca6405fc73aca26f38767b1114f42
独泪了无痕 <duleilewuhen@sina.com>
2018-06-13 23:52:50 +0800

    增加容错

--- wechat-utils/src/main/java/com/cdeledu/util/network/ftp/SftpHelper.java
+++ wechat-utils/src/main/java/com/cdeledu/util/network/ftp/SftpHelper.java
@@ -2,6 +2,8 @@ package com.cdeledu.util.network.ftp;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.Vector;
 
@@ -9,13 +11,13 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.google.common.collect.Lists;
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.ChannelSftp.LsEntry;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
-import com.jcraft.jsch.SftpATTRS;
 import com.jcraft.jsch.SftpException;
 
 /**
@@ -29,6 +31,9 @@ import com.jcraft.jsch.SftpException;
  *  sftp是Secure File Transfer Protocol的缩写，安全文件传送协议。
  *       </pre>
  * 
+ *       <p>
+ *       1.所有的文件路径必须以'/'开头和结尾，否则路径最后一部分会被当做是文件名<br>
+ *       </p>
  * @创建者: 独泪了无痕--duleilewuhen@sina.com
  * @创建时间: 2018年6月10日 下午10:12:29
  * @版本: V1.0
@@ -53,9 +58,6 @@ public class SftpHelper {
 	private static ChannelSftp sftpClient;
 	private Session sshSession;
 	private Channel channel;
-	public int conFTPTryCount = 0;
-	public int conFTPTryInterval = 1;
-	public Boolean isConnected = false;
 
 	/** ----------------------------------------------------- Fields end */
 
@@ -71,7 +73,7 @@ public class SftpHelper {
 		this.sftpPassword = sftpPassword;
 		this.sftpHost = sftpHost;
 		this.sftpPort = sftpPort;
-		getSftpConnect();
+		init();
 	}
 
 	/**
@@ -88,16 +90,42 @@ public class SftpHelper {
 		this.sftpPort = sftpPort;
 		this.privateKey = privateKey;
 		this.passphrase = passphrase;
-		getSftpConnect();
+		init();
 	}
 
 	/**
-	 * @方法:连接sftp服务器
+	 * @方法:关闭协议-sftp协议.(关闭会导致连接池异常，因此不建议用户自定义关闭)
 	 * @创建人:独泪了无痕
-	 * @throws SftpException,JSchException
-	 * @throws JSchException
 	 */
-	private ChannelSftp getSftpConnect() throws SftpException, JSchException {
+	private void logout() {
+		if (sftpClient != null) {
+			if (sftpClient.isConnected()) {
+				sftpClient.disconnect();
+			}
+		}
+	}
+
+	/**
+	 * @方法:路径
+	 * @创建人:独泪了无痕
+	 * @param srcPath
+	 *            原路径. /xxx/xxx/xxx.yyy 或 X:/xxx/xxx/xxx.yy
+	 * @return 第一个是路径（/xxx/xxx/）,第二个是文件名（xxx.yy）
+	 */
+	private static List<String> formatPath(final String srcPath) {
+		List<String> list = new ArrayList<String>(2);
+		String repSrc = srcPath.replaceAll("\\\\", "/");
+		int firstP = repSrc.indexOf("/");
+		int lastP = repSrc.lastIndexOf("/");
+		String fileName = lastP < 0 ? repSrc : repSrc.substring(lastP + 1);
+		String dir = firstP < 0 ? "" : repSrc.substring(firstP, lastP);
+		dir = (dir.length() == 1 ? dir : (dir + "/"));
+		list.add(dir);
+		list.add(fileName);
+		return list;
+	}
+
+	private void init() throws SftpException, JSchException {
 		jsch = new JSch();
 
 		if (sftpClient == null) {
@@ -137,19 +165,91 @@ public class SftpHelper {
 			}
 
 		}
+	}
+
+	private void getSftpSession() throws SftpException, JSchException {
+		if (sshSession == null || !sshSession.isConnected()) {
+			synchronized (Session.class) {
+				if (StringUtils.isNoneBlank(privateKey)) {
+					// 使用密钥验证方式，密钥可以使有口令的密钥，也可以是没有口令的密钥
+					if (StringUtils.isNoneBlank(passphrase)) {
+						jsch.addIdentity(privateKey, passphrase);
+					} else {
+						jsch.addIdentity(privateKey);
+					}
+				}
+				// 根据用户名、主机ip、端口连接SFTP服务器,获取一个Session对象
+				sshSession = jsch.getSession(sftpUserName, sftpHost, sftpPort);
+				if (StringUtils.isNoneBlank(sftpPassword)) {
+					// 设置密码
+					sshSession.setPassword(sftpPassword);
+				}
+				Properties sshConfig = new Properties();
+				sshConfig.put("StrictHostKeyChecking", "no");
+
+				sshSession.setConfig(sshConfig);
+				sshSession.setServerAliveInterval(92000);
+				sshSession.connect();
+			}
+		}
+	}
+
+	private void getSftpChannel() throws SftpException, JSchException {
+		if (channel == null || channel.isClosed()) {
+			synchronized (Channel.class) {
+				getSftpSession();
+				// 参数sftp指明要打开的连接是sftp连接
+				channel = sshSession.openChannel("sftp");
+				channel.connect();
+				sftpClient = (ChannelSftp) channel;
+			}
+		}
+	}
+
+	/**
+	 * @方法:连接sftp服务器
+	 * @创建人:独泪了无痕
+	 * @throws SftpException,JSchException
+	 * @throws JSchException
+	 */
+	private ChannelSftp getSftpConnect() throws SftpException, JSchException {
+
+		if (sftpClient == null) {
+			synchronized (SftpHelper.class) {
+				// 参数sftp指明要打开的连接是sftp连接
+				getSftpChannel();
+				channel.connect();
+				sftpClient = (ChannelSftp) channel;
+			}
+
+		}
 		return sftpClient;
 	}
 
 	/**
-	 * @方法:关闭协议-sftp协议.(关闭会导致连接池异常，因此不建议用户自定义关闭)
+	 * @方法描述 : 判断文件夹是否存在
+	 * @param dir
+	 *            文件夹路径， /xxx/xxx/
+	 * @return
+	 * @throws SftpException
+	 */
+	private boolean isDirExist(final String dir) throws SftpException, JSchException {
+		Vector<?> vector = getSftpConnect().ls(dir);
+		if (null == vector) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * @方法:退出
 	 * @创建人:独泪了无痕
 	 */
-	private void logout() {
+	public void exit() {
 		if (sftpClient != null) {
 			if (sftpClient.isConnected()) {
 				sftpClient.disconnect();
-				// sftp.exit();
-				// sftp.quit();
 			}
 		}
 		if (sshSession != null) {
@@ -178,26 +278,25 @@ public class SftpHelper {
 	 * @throws SftpException
 	 * @throws JSchException
 	 */
-	public boolean upload(final String srcFile, final String basePath, final String directory,
-			final String sftpFileName) throws SftpException, JSchException {
+	public boolean upload(final String srcFile, final String directory, final String sftpFileName)
+			throws SftpException, JSchException {
+
 		boolean result = false;
-		if (StringUtils.isNoneEmpty(basePath)) {
-			try {
-				sftpClient.cd(basePath);
-				if (StringUtils.isNotBlank(directory)) {
-					sftpClient.cd(directory);
-				}
-				// 目录不存在，则创建文件夹
-				// mkdirs(basePath + "/" + directory);
+		// 源文件必须存在、基础路径不为空、是文件夹路径
+		File file = new File(srcFile);
+		try {
+			if (file.exists()) {
+				mkdir(directory);
+				getSftpConnect().cd(directory);
 				if (StringUtils.isNotBlank(sftpFileName)) {
-					sftpClient.put(srcFile, sftpFileName);
+					getSftpConnect().put(srcFile, sftpFileName);
 				} else {
-					sftpClient.put(srcFile);
+					getSftpConnect().put(srcFile);
 				}
 				result = true;
-			} finally {
-				logout();
 			}
+		} finally {
+			logout();
 		}
 		return result;
 	}
@@ -215,26 +314,26 @@ public class SftpHelper {
 	 * @throws SftpException
 	 * @throws IOException
 	 */
-	public void download(final String directory, final String downloadFile, final String saveFile)
+	public boolean download(final String downloadFile, final String saveFile)
 			throws SftpException, JSchException, IOException {
 		File file = new File(saveFile);
-		if (StringUtils.isNotBlank(directory)) { // 下载目录不为空
-			try {
-				if (!file.exists()) {
-					File parentFile = file.getParentFile();
-					if (!parentFile.exists()) {
-						parentFile.mkdirs();
-					}
-					file.createNewFile();
+		boolean result = false;
+		try {
+			if (!file.exists()) {
+				File parentFile = file.getParentFile();
+				if (!parentFile.exists()) {
+					parentFile.mkdirs();
 				}
-				// 目录不存在，则创建文件夹
-				sftpClient.cd(directory);
-				sftpClient.get(downloadFile, saveFile);
-				// IOUtils.toByteArray(sftp.get(downloadFile));
-			} finally {
-				logout();
+				file.createNewFile();
 			}
+			List<String> srcList = formatPath(downloadFile);
+			getSftpConnect().cd(srcList.get(0));
+			getSftpConnect().get(srcList.get(1), saveFile);
+			result = true;
+		} finally {
+			logout();
 		}
+		return result;
 	}
 
 	/**
@@ -246,16 +345,19 @@ public class SftpHelper {
 	 *            要删除的文件
 	 * @return 成功返回true，否则返回false
 	 * @throws SftpException
+	 * @throws JSchException
 	 */
-	public boolean delete(final String directory, final String deleteFile) throws SftpException {
+	public boolean rmFile(final String deleteFile) throws SftpException, JSchException {
 		boolean result = false;
-		if (StringUtils.isNotBlank(directory) && StringUtils.isNotBlank(deleteFile)) {
-			try {
-				sftpClient.cd(directory);
-				sftpClient.rm(deleteFile);
-			} finally {
-				logout();
+		List<String> srcList = formatPath(deleteFile);
+		try {
+			if (isDirExist(srcList.get(0))) {
+				getSftpConnect().cd(srcList.get(0));
+				getSftpConnect().rm(srcList.get(1));
+				result = true;
 			}
+		} finally {
+			logout();
 		}
 		return result;
 	}
@@ -271,10 +373,15 @@ public class SftpHelper {
 	public boolean rmDir(final String directory, final boolean recursion) throws Exception {
 		boolean result = false;
 		try {
-			if (!recursion) {
-
-			} else {
-				exeRmRec(directory);
+			String fp = formatPath(directory).get(0);
+			if (isDirExist(fp)) {
+				if (recursion) {
+					exeRmRec(fp);
+					result = true;
+				} else {
+					getSftpConnect().rmdir(fp);
+					result = true;
+				}
 			}
 		} finally {
 			logout();
@@ -287,15 +394,16 @@ public class SftpHelper {
 	 * @param directory
 	 *            文件路径
 	 * @throws SftpException
+	 * @throws JSchException
 	 */
 	@SuppressWarnings("unchecked")
-	public void exeRmRec(final String directory) throws SftpException {
+	public void exeRmRec(final String directory) throws SftpException, JSchException {
 		try {
-			Vector<LsEntry> vector = sftpClient.ls(directory);
+			Vector<LsEntry> vector = getSftpConnect().ls(directory);
 			if (vector.size() == 1) { // 文件，直接删除
-				sftpClient.rm(directory);
+				getSftpConnect().rm(directory);
 			} else if (vector.size() == 2) { // 空文件夹，直接删除
-				sftpClient.rmdir(directory);
+				getSftpConnect().rmdir(directory);
 			} else {
 				String fileName;
 				// 删除文件夹下所有文件
@@ -307,11 +415,12 @@ public class SftpHelper {
 						exeRmRec(directory + "/" + fileName);
 					}
 				}
+				// 删除文件夹
+				sftpClient.rmdir(directory);
 			}
 		} finally {
 			logout();
 		}
-
 	}
 
 	/**
@@ -319,31 +428,32 @@ public class SftpHelper {
 	 * @创建人:独泪了无痕
 	 * @param directory
 	 *            要列出的目录
+	 * @throws JSchException
 	 */
 	@SuppressWarnings("unchecked")
-	public void getFileLists(String directory) {
+	public List<String> getFileLists(String directory) throws JSchException {
+		List<String> resultList = Lists.newArrayList();
 		if (StringUtils.isBlank(directory)) {
-
+			return null;
 		}
 		if (directory.endsWith("/")) {
 			directory = directory + "/";
 		}
 		try {
-			Vector<LsEntry> vector = sftpClient.ls(directory);
+			Vector<LsEntry> vector = getSftpConnect().ls(directory);
 			String fileName;
 			for (LsEntry entry : vector) {
 				fileName = entry.getFilename();
 				if (fileName.equals(".") || fileName.equals("..")) {
 					continue;
 				}
-				SftpATTRS attrs = entry.getAttrs();
-				System.out.println(fileName);
-				System.out.println(attrs.isDir()); // 是否是文件夹
-				System.out.println(attrs.isFifo());
-				System.out.println(attrs.getSize());// 文件大小
-				System.out.println(attrs.getATime());// 创建时间
-				System.out.println(attrs.getMTime());// 最后更新时间
-				System.out.println("--------------");
+				resultList.add(fileName);
+				// SftpATTRS attrs = entry.getAttrs();
+				// attrs.isDir(); // 是否是文件夹
+				// attrs.isFifo();
+				// attrs.getSize();// 文件大小
+				// attrs.getATime();// 创建时间
+				// attrs.getMTime();// 最后更新时间
 			}
 
 		} catch (SftpException e) {
@@ -351,6 +461,7 @@ public class SftpHelper {
 		} finally {
 			logout();
 		}
+		return resultList;
 	}
 
 	/**
@@ -358,22 +469,19 @@ public class SftpHelper {
 	 * @param dir
 	 *            路径 必须是 /xxx/xxx/xxx/ 不能就单独一个/
 	 * @return 是否创建成功
+	 * @throws SftpException
+	 * @throws JSchException
 	 * @throws Exception
 	 */
-	public boolean mkdir(final String dir) throws Exception {
-		boolean result = false;
-		try {
-			if (StringUtils.isBlank(dir)) {
-				return false;
-			}
-			String md = dir.replaceAll("\\\\", "/");
-			if (md.indexOf("/") != 0 || md.length() == 1) {
-				mkdirs(md);
-			}
-		} finally {
-			logout();
+	public boolean mkdir(final String dir) throws SftpException, JSchException {
+		if (StringUtils.isBlank(dir)) {
+			return false;
 		}
-		return result;
+		String md = dir.replaceAll("\\\\", "/");
+		if (md.indexOf("/") != 0 || md.length() == 1) {
+			return false;
+		}
+		return mkdirs(md);
 	}
 
 	/**
@@ -383,37 +491,20 @@ public class SftpHelper {
 	 *            路径
 	 * @return 是否创建成功
 	 * @throws SftpException
+	 * @throws JSchException
 	 */
-	public boolean mkdirs(final String dir) throws SftpException {
-		boolean result = false;
-		try {
-
-		} finally {
-			logout();
-		}
-		return result;
-	}
-
-	/**
-	 * @方法描述 : 判断文件夹是否存在
-	 * @param dir
-	 *            文件夹路径， /xxx/xxx/
-	 * @return
-	 */
-	public boolean isDirExist(final String dir) {
-		boolean result = false;
-		try {
-			Vector<?> vector = sftpClient.ls(dir);
-			if (null == vector) {
-				result = false;
+	public boolean mkdirs(final String dir) throws SftpException, JSchException {
+		String dirs = dir.substring(1, dir.length() - 1);
+		String[] dirArr = dirs.split("/");
+		String base = "";
+		for (String d : dirArr) {
+			base += "/" + d;
+			if (isDirExist(base + "/")) {
+				continue;
 			} else {
-				result = true;
+				getSftpConnect().mkdir(base + "/");
 			}
-		} catch (SftpException e) {
-			e.printStackTrace();
-		} finally {
-			logout();
 		}
-		return result;
+		return true;
 	}
 }
