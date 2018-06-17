ae08f4d593aae8e0689a69818914eabb3bc38bd1
独泪了无痕 <duleilewuhen@sina.com>
2018-06-18 00:35:59 +0800

    替换js获取项目根路径，如果项目server.xml 中的 context 配置了 path ,一旦请求层级超过两级，获得结果是不符合的！

--- wechat-manage/src/main/webapp/plug-in/tools/extJavascript.js
+++ wechat-manage/src/main/webapp/plug-in/tools/extJavascript.js
@@ -52,16 +52,14 @@ $.ajaxSetup({
  * js获取项目根路径
  */
 dllwh.getRootPath = function() {
-	// 获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
-	var curWwwPath = window.document.location.href;
-	// 获取主机地址之后的目录，如： uimcardprj/share/meun.jsp
-	var pathName = window.document.location.pathname;
-	var pos = curWwwPath.indexOf(pathName);
-	// 获取主机地址，如： http://localhost:8083
-	var localhostPaht = curWwwPath.substring(0, pos);
-	// 获取带"/"的项目名，如：/uimcardprj
-	var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
-	return (localhostPaht + projectName) + "/";
+	var pathName = window.location.pathname.substring(1);
+	var webName = pathName == '' ? '' : pathName.substring(0, pathName.indexOf('/'));
+	if (webName == "") {
+		return window.location.protocol + '//' + window.location.host + '/';
+	}
+	else {
+		return window.location.protocol + '//' + window.location.host + '/' + webName + '/';
+	}
 }
 
 /**
