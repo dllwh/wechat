720ca7f5321096275f8ba98c7db60c4482d27ee1
独泪了无痕 <duleilewuhen@sina.com>
2018-06-27 00:38:33 +0800

    工作累

---
+++ wechat-utils/src/main/java/com/cdeledu/util/application/cdel/CdelLiveHelper.java
@@ -0,0 +1,19 @@
+package com.cdeledu.util.application.cdel;
+
+/**
+ * 
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 正保直播
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年6月27日 上午12:29:40
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public final class CdelLiveHelper {
+	/** ----------------------------------------------------- Fields start */
+
+	/** ----------------------------------------------------- Fields end */
+}
