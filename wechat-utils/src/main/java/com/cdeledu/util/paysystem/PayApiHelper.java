5d8308f50213c27b3d0a3cf26d4ecd73f1c3fa21
独泪了无痕 <duleilewuhen@sina.com>
2018-07-05 23:22:44 +0800

    创建支付模块

---
+++ wechat-utils/src/main/java/com/cdeledu/util/paysystem/PayApiHelper.java
@@ -0,0 +1,18 @@
+package com.cdeledu.util.paysystem;
+
+/***
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 支付基类
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年7月5日 下午11:20:29
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public class PayApiHelper {
+	/** ----------------------------------------------------- Fields start */
+
+	/** ----------------------------------------------------- Fields end */
+}
