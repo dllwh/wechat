aabede7390d526c9ab440c84a7414d0fd36b58f3
独泪了无痕 <duleilewuhen@sina.com>
2018-07-12 20:58:48 +0800

    更新

--- wechat-utils/src/main/java/com/cdeledu/util/application/email/ReplyMailHelper.java
+++ wechat-utils/src/main/java/com/cdeledu/util/application/email/ReplyMailHelper.java
@@ -1,5 +1,7 @@
 package com.cdeledu.util.application.email;
 
+import javax.mail.Message;
+
 /**
  * @Description: 邮件的回复
  * @author: 独泪了无痕
@@ -7,5 +9,20 @@ package com.cdeledu.util.application.email;
  * @version: V1.0
  * @history:
  */
-class ReplyMailHelper {
+final class ReplyMailHelper {
+	/**
+	 * @方法描述 : 邮件回复方法
+	 * @思路：先获取要回复的邮件，然后获取邮件的各个详情，把详情附加给新建邮件，发送
+	 */
+	public static void replyEmail() {
+		newReplyMessage();
+	}
+
+	/**
+	 * @方法描述 :组装要回复的邮件
+	 * @return
+	 */
+	public static Message newReplyMessage() {
+		return null;
+	}
 }
