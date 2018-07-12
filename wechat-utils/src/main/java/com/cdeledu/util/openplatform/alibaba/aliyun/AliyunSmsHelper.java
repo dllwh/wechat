26d6e28cdd71addffae45fd8cbe5597dbf7aa95d
独泪了无痕 <duleilewuhen@sina.com>
2018-07-12 11:29:50 +0800

    短信服务

---
+++ wechat-utils/src/main/java/com/cdeledu/util/openplatform/alibaba/aliyun/AliyunSmsHelper.java
@@ -0,0 +1,23 @@
+package com.cdeledu.util.openplatform.alibaba.aliyun;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述:
+ *       <p>
+ *       短信服务（Short Message Service）是指通过调用短信发送API，将指定短信内容发送给指定手机用户。s
+ *       用户收到的短信来自106开头的号码，短信的内容多用于企业向用户传递验证码、系统通知、会员服务等信息。
+ *       </p>
+ * @创建者: 皇族灬战狼
+ * @联系方式: duleilewuhen@sina.com
+ * @创建时间: 2018年7月12日 上午10:29:55
+ * @版本: V1.0
+ * @since: JDK 1.7
+ * @see <a href="help.aliyun.com/product/44282.html">短信服务</a>
+ */
+public final class AliyunSmsHelper {
+	/** ----------------------------------------------------- Fields start */
+	/** ----------------------------------------------------- Fields end */
+}
