8057dcc11c64ac061e6242196376d673bf88d9b2
独泪了无痕 <duleilewuhen@sina.com>
2018-07-18 21:39:11 +0800

    mongo 单机 帮助类

---
+++ wechat-utils/src/main/java/com/cdeledu/util/database/mongodb/MongodbSingleHelper.java
@@ -0,0 +1,18 @@
+package com.cdeledu.util.database.mongodb;
+
+/**
+ * 
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: mongo 单机 帮助类
+ * @创建者: 皇族灬战狼
+ * @联系方式: duleilewuhen@sina.com
+ * @创建时间: 2018年7月17日 下午5:29:15
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public final class MongodbSingleHelper {
+
+}
