ce461f5aac3991393bf028c52c422f95bba5230a
独泪了无痕 <duleilewuhen@sina.com>
2018-07-18 21:36:53 +0800

    Mongo 配置工厂

---
+++ wechat-utils/src/main/java/com/cdeledu/util/database/mongodb/factory/MongoConfigFactory.java
@@ -0,0 +1,19 @@
+package com.cdeledu.util.database.mongodb.factory;
+
+/**
+ * 
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: Mongo 配置工厂
+ * @创建者: 皇族灬战狼
+ * @联系方式: duleilewuhen@sina.com
+ * @创建时间: 2018年7月18日 下午9:33:53
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public class MongoConfigFactory {
+	/** ----------------------------------------------------- Fields start */
+	/** ----------------------------------------------------- Fields end */
+}
