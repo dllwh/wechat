8e728b361d3569257c2634d0c2d053e4a23327a6
独泪了无痕 <duleilewuhen@sina.com>
2018-07-18 21:24:56 +0800

    增加获取数据

--- wechat-manage/src/main/java/com/cdeledu/controller/cms/ArticleController.java
+++ wechat-manage/src/main/java/com/cdeledu/controller/cms/ArticleController.java
@@ -1,10 +1,16 @@
 package com.cdeledu.controller.cms;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cdeledu.controller.BaseController;
+import com.cdeledu.model.cms.notice.Article;
+import com.cdeledu.service.cms.NoticeService;
 
 /**
  * 
@@ -23,8 +29,11 @@ import com.cdeledu.controller.BaseController;
 public class ArticleController extends BaseController {
 	/** ----------------------------------------------------- Fields start */
 	private static final long serialVersionUID = 1L;
+	@Autowired
+	private NoticeService noticeService;
+
 	/** ----------------------------------------------------- Fields end */
-	
+
 	/**
 	 * @方法描述: 文章初始化页面
 	 * @return
@@ -33,7 +42,18 @@ public class ArticleController extends BaseController {
 	public String index(ModelMap modelMap) {
 		return "cms/article/articleInit";
 	}
-	
+
+	@ResponseBody
+	@RequestMapping(value = "index", params = "getList")
+	public List<Article> getList(ModelMap modelMap, Article article) {
+		try {
+			return noticeService.getArticleList(article);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 	/**
 	 * @方法描述: 文章
 	 * @return
