5db26e0d6ef0502e104fa4a2e0cac6ef254212cd
独泪了无痕 <duleilewuhen@sina.com>
2018-07-18 21:37:57 +0800

    mongodb集群帮助类

---
+++ wechat-utils/src/main/java/com/cdeledu/util/database/mongodb/MongodbClustetHelper.java
@@ -0,0 +1,19 @@
+package com.cdeledu.util.database.mongodb;
+
+/**
+ * 
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: mongodb集群帮助类
+ * @创建者: 皇族灬战狼
+ * @联系方式: duleilewuhen@sina.com
+ * @创建时间: 2018年7月17日 下午5:37:13
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public final class MongodbClustetHelper {
+	/** ----------------------------------------------------- Fields start */
+	/** ----------------------------------------------------- Fields end */
+}
