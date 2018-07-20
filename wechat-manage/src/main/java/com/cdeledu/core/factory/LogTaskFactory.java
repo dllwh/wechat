54e74f89fc00287a54b7eb8efc090e6e7adefc14
独泪了无痕 <duleilewuhen@sina.com>
2018-07-20 11:10:23 +0800

    优化代码并去除一些多余的注释说明

--- wechat-manage/src/main/java/com/cdeledu/core/factory/LogTaskFactory.java
+++ wechat-manage/src/main/java/com/cdeledu/core/factory/LogTaskFactory.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.JoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.cdeledu.core.log.LogManager;
 import com.cdeledu.service.log.LoginLogService;
 import com.cdeledu.service.log.OperateLogService;
 
@@ -15,18 +14,16 @@ import com.cdeledu.service.log.OperateLogService;
  *
  * Today the best performance as tomorrow newest starter!
  *
- * @类描述: 日志操作任务创建工厂
+ * @类描述: 日志操作任务工厂
  * @创建者: 皇族灬战狼
  * @创建时间: 2017年12月9日 下午2:53:51
- * @版本: V1.0
+ * @版本: V1.1
  * @since: JDK 1.7
  */
 public class LogTaskFactory {
-	private static Logger logger = LoggerFactory.getLogger(LogManager.class);
+	private static Logger logger = LoggerFactory.getLogger(LogTaskFactory.class);
 	private static OperateLogService operateLogService = ConstantFactory.operateLogService;
 	private static LoginLogService loginLogService = ConstantFactory.loginLogService;
-
-	/** ----------------------------------------------- [公共方法] */
 
 	public static TimerTask operateLog(final JoinPoint point, final long time, final Throwable e,
 			final Object opResult, final String ip, final String browser) {
@@ -37,7 +34,7 @@ public class LogTaskFactory {
 					operateLogService.addLog(
 							LogFactory.createOperateLog(point, time, e, opResult, ip, browser));
 				} catch (Exception e) {
-					logger.error("创建退出日志异常!", e);
+					logger.error("保存日常操作日志异常!", e);
 				}
 			}
 		};
@@ -52,17 +49,9 @@ public class LogTaskFactory {
 					loginLogService.insert(
 							LogFactory.createLoginLog(userCode, content, status, ip, browser));
 				} catch (Exception e) {
-					logger.error("创建退出日志异常!", e);
+					logger.error("保存登录日志异常!", e);
 				}
 			}
 		};
 	}
-
-	/** ----------------------------------------------- [公共方法] */
-
-	/** ----------------------------------------------- [私有方法] */
-	/** ----------------------------------------------- [私有方法] */
-
-	/** ----------------------------------------------- [测试方法] */
-	/** ----------------------------------------------- [测试方法] */
 }
