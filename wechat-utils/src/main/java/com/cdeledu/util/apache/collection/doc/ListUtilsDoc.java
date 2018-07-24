7f202b44ddaeb3cad4292d3f5b0dda7ed2e3c188
独泪了无痕 <duleilewuhen@sina.com>
2018-07-24 15:58:32 +0800

    更新

--- wechat-utils/src/main/java/com/cdeledu/util/apache/collection/doc/ListUtilsDoc.java
+++ wechat-utils/src/main/java/com/cdeledu/util/apache/collection/doc/ListUtilsDoc.java
@@ -7,8 +7,9 @@ package com.cdeledu.util.apache.collection.doc;
  * @创建日期: 2015年8月29日 下午2:22:50
  * @版本: V1.0
  * @since: JDK 1.7
+ * @see <a href="commons.apache.org/proper/commons-collections/javadocs/"></a>
  * @see <a href=
- *      "http://commons.apache.org/proper/commons-collections/javadocs/api-release/org/apache/commons/collections4/">
+ *      "commons.apache.org/proper/commons-collections/javadocs/api-4.0/org/apache/commons/collections4/">
  *      org.apache.commons.collections4</a>
  */
 interface ListUtilsDoc {
