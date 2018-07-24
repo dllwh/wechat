2272b1c2936f543938bd5ba228693c240d8e317d
独泪了无痕 <duleilewuhen@sina.com>
2018-07-25 00:08:07 +0800

    世纪佳缘篇

---
+++ wechat-webCrawler/src/main/java/com/cdeledu/crawler/SocialNetwork/marryandlove/JiayuanHelper.java
@@ -0,0 +1,19 @@
+package com.cdeledu.crawler.SocialNetwork.marryandlove;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 自娱自乐写爬虫 世纪佳缘篇
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年7月24日 下午11:34:15
+ * @版本: V1.0
+ * @since: JDK 1.7
+ * @see <a href="www.cnblogs.com/lrysjtu/p/4474900.html">参考文档</a>
+ */
+public final class JiayuanHelper {
+	/** ----------------------------------------------------- Fields start */
+
+	/** ----------------------------------------------------- Fields end */
+}
