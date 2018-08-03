f206696dd4489bd08e4ee9f693de7610dd1617fb
独泪了无痕 <duleilewuhen@sina.com>
2018-08-03 17:37:54 +0800

    消息类型

---
+++ wechat-utils/src/main/java/com/cdeledu/util/openplatform/douyutv/constants/MsgType.java
@@ -0,0 +1,55 @@
+package com.cdeledu.util.openplatform.douyutv.constants;
+
+/**
+ * 
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 消息类型
+ * @创建者: 皇族灬战狼
+ * @联系方式: duleilewuhen@sina.com
+ * @创建时间: 2018年8月1日 上午11:07:20
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public final class MsgType {
+	// 登录响应
+	public static final String LOGIN_RES = "loginres";
+
+	// 服务心跳响应
+	public static final String KEEP_LIVE = "loginres";
+
+	// 弹幕消息
+	public static final String CHAT_MSG = "chatmsg";
+
+	// 领取鱼丸暴击
+	public static final String ONLINE_GIFT = "onlinegift";
+
+	// 赠送礼物消息
+	public static final String DGB = "dgb";
+
+	// 用户进入房间
+	public static final String UENTER = "uenter";
+
+	// 房间开播关播
+	public static final String RSS = "rss";
+
+	// 房间贡献排行榜更新广播
+	public static final String RANK_LIST = "ranklist";
+
+	// 超级弹幕
+	public static final String SSD = "ssd";
+
+	// 房间内礼物广播
+	public static final String SPBC = "spbc";
+
+	// 房间用户抢红包
+	public static final String GGBB = "ggbb";
+
+	// 房间分区排名变化消息
+	public static final String RANK_UP = "rankup";
+
+	// 错误信息
+	public static final String ERROR = "error";
+}
