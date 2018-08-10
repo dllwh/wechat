f9d2f39ce142309b3b9f04bf5ab24e68ba3d7e19
独泪了无痕 <duleilewuhen@sina.com>
2018-08-10 20:31:10 +0800

    增加服务端小熊类型

--- wechat-utils/src/main/java/com/cdeledu/util/openplatform/douyutv/constants/MsgType.java
+++ wechat-utils/src/main/java/com/cdeledu/util/openplatform/douyutv/constants/MsgType.java
@@ -6,50 +6,64 @@ package com.cdeledu.util.openplatform.douyutv.constants;
  *
  * Today the best performance as tomorrow newest starter!
  *
- * @类描述: 消息类型
+ * @类描述: 服务端向客户端发送消息的消息类型
  * @创建者: 皇族灬战狼
  * @联系方式: duleilewuhen@sina.com
  * @创建时间: 2018年8月1日 上午11:07:20
- * @版本: V1.0
+ * @版本: V1.1
  * @since: JDK 1.7
  */
 public final class MsgType {
 	// 登录响应
 	public static final String LOGIN_RES = "loginres";
-
 	// 服务心跳响应
 	public static final String KEEP_LIVE = "loginres";
-
 	// 弹幕消息
 	public static final String CHAT_MSG = "chatmsg";
-
 	// 领取鱼丸暴击
 	public static final String ONLINE_GIFT = "onlinegift";
-
 	// 赠送礼物消息
 	public static final String DGB = "dgb";
-
 	// 用户进入房间
 	public static final String UENTER = "uenter";
-
 	// 房间开播关播
 	public static final String RSS = "rss";
-
 	// 房间贡献排行榜更新广播
 	public static final String RANK_LIST = "ranklist";
-
 	// 超级弹幕
 	public static final String SSD = "ssd";
-
 	// 房间内礼物广播
 	public static final String SPBC = "spbc";
-
 	// 房间用户抢红包
 	public static final String GGBB = "ggbb";
-
 	// 房间分区排名变化消息
 	public static final String RANK_UP = "rankup";
-
 	// 错误信息
 	public static final String ERROR = "error";
+	// 主播离开提醒
+	public static final String AL = "al";
+	// 主播回来继续直播提醒
+	public static final String AB = "ab";
+	// 用户点赞推送通知消息
+	public static final String RUCLP = "ruclp";
+	// 房间内用户抢到道具消息
+	public static final String GPBC = "gpbc";
+	// 房间贵族列表广播消息
+	public static final String ONLINE_NOBLE_LIST = "online_noble_list";
+	// 用户等级提升消息
+	public static final String UPGRADE = "upgrade";
+	// 房间用户等级排行榜消息
+	public static final String UL_RANKLIST = "ul_ranklist";
+	// 主播等级提升广播
+	public static final String UPBC = "upbc";
+	// 禁言操作结果
+	public static final String NEWBLACKRES = "newblackres";
+	// 徽章等级提升通知
+	public static final String BLAB = "blab";
+	// 粉丝排行榜消息
+	public static final String FRANK = "frank";
+	// 用户分享了直播间通知
+	public static final String SRRES = "srres";
+	// 栏目排行榜变更通知
+	public static final String RRI = "rri";
 }
