5edb77b6d6977975b19193e4e87087a64979dc60
独泪了无痕 <duleilewuhen@sina.com>
2018-08-17 22:59:11 +0800

    斗鱼常量

---
+++ wechat-utils/src/main/java/com/cdeledu/util/openplatform/douyutv/constants/DouYuConstants.java
@@ -0,0 +1,23 @@
+package com.cdeledu.util.openplatform.douyutv.constants;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 斗鱼常量
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年8月17日 下午10:53:17
+ * @版本: V1.0
+ * @since: JDK 1.7
+ */
+public final class DouYuConstants {
+	/** ----------------------------------------------------- Fields start */
+	// 海量弹幕分组
+	public final static String MASSIVE_GID = "-9999";
+	// 客户端发送给弹幕服务器的文本格式数据
+	public final static int MESSAGE_TYPE_CLIENT = 689;
+	// 弹幕服务器发送给客户端的文本格式数据
+	public final static int MESSAGE_TYPE_SERVER = 690;
+	/** ----------------------------------------------------- Fields end */
+}
