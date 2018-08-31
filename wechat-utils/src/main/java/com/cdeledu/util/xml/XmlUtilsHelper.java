b963ef236595843cf4a71210010ef6dfe8166a91
独泪了无痕 <duleilewuhen@sina.com>
2018-08-31 14:57:04 +0800

    版本1.1.1

--- wechat-utils/src/main/java/com/cdeledu/util/xml/XmlUtilsHelper.java
+++ wechat-utils/src/main/java/com/cdeledu/util/xml/XmlUtilsHelper.java
@@ -4,7 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
@@ -13,12 +13,14 @@ import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
+import com.google.common.collect.Maps;
+
 /**
  * @ClassName: XmlUtilsHelper
  * @Description: xml与其他文件的相关操作(外部)
  * @author: 独泪了无痕
  * @date: 2015年9月18日 上午11:35:43
- * @version: V1.1
+ * @version: V1.1.1
  * @history:
  */
 public class XmlUtilsHelper {
@@ -32,27 +34,41 @@ public class XmlUtilsHelper {
 	 * @throws IOException
 	 * @throws DocumentException
 	 */
-	public static Map<String, Object> parseXml(InputStream inputStream)
-			throws Exception, IOException, DocumentException {
+	public static Map<String, Object> parseXml(InputStream inputStream) throws Exception {
 		// 将解析结果存放在HashMap中
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 
 		SAXReader reader = new SAXReader();
-		Document document = reader.read(inputStream);
-		// 得到xml根元素
-		Element root = document.getRootElement();
-		// 获取根元素的所有子节点
-		@SuppressWarnings("unchecked")
-		List<Element> elementList = root.elements();
-		// 遍历所有子节点
-		for (Element ele : elementList) {
-			resultMap.put(ele.getName(), ele.getTextTrim());
+		Document document = reader.read(inputStream);// 得到Document对象
+		Element root = document.getRootElement(); // 得到xml根元素
+		Iterator<?> iterator = root.elementIterator(); // 遍历其子节点
+		while (iterator.hasNext()) {
+			Element child = (Element) iterator.next();
+			if (child.isTextOnly()) {
+				resultMap.put(child.getName(), child.getTextTrim());
+			} else {
+				resultMap.put(child.getName(), parseMap(child));
+			}
 		}
 		return resultMap;
 	}
 
-	public static Map<String, Object> parseXml(String content, String charset)
-			throws Exception, IOException, DocumentException {
+	private static Map<String, Object> parseMap(Element rootElement) throws Exception {
+		Map<String, Object> resultMap = Maps.newHashMap();
+		// List<Element> elementList = rootElement.elements();
+		Iterator<?> iterator = rootElement.elementIterator();
+		while (iterator.hasNext()) {
+			Element ele = (Element) iterator.next();
+			if (ele.isTextOnly()) {
+				resultMap.put(ele.getName(), ele.getTextTrim());
+			} else {
+				resultMap.put(ele.getName(), parseMap(ele));
+			}
+		}
+		return resultMap;
+	}
+
+	public static Map<String, Object> parseXml(String content, String charset) throws Exception {
 		InputStream inputStream = null;
 		if (StringUtils.isNotBlank(charset)) {
 			inputStream = new ByteArrayInputStream(content.getBytes(charset));
