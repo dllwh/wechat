46491323f27caae32dc72077a4a742d306e4ea6f
独泪了无痕 <duleilewuhen@sina.com>
2018-09-06 00:22:29 +0800

    替换响应实体类

--- wechat-common/src/main/java/com/cdeledu/common/base/ResponseBean.java
+++ wechat-common/src/main/java/com/cdeledu/common/base/ResponseBean.java
@@ -1,22 +1,29 @@
 package com.cdeledu.common.base;
 
-import java.io.Serializable;
 import java.util.Map;
 
 /**
- * @类描述: $.ajax后需要接受的JSON:封装Ajax结果,所有Ajax请求返回类型
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 响应实体
  * @创建者: 独泪了无痕--duleilewuhen@sina.com
- * @创建日期: 2016年4月4日 下午7:05:53
+ * @创建时间: 2018年9月5日 下午10:54:23
  * @版本: V1.0
  * @since: JDK 1.7
  */
-public class AjaxJson implements Serializable {
-	private static final long serialVersionUID = 1L;
+public final class ResponseBean {
 	private boolean success = true;// 是否成功
 	private Integer resultCode = 200;// 状态码，默认是200
 	private String msg = "操作成功";// 提示信息
 	private Object obj = null;// 其他信息
 	private Map<String, Object> attributes;// 其他参数
+
+	public ResponseBean() {
+		this.success = true;
+		this.resultCode = 200;
+	}
 
 	public boolean isSuccess() {
 		return success;
@@ -63,5 +70,4 @@ public class AjaxJson implements Serializable {
 		return "AjaxJson [success=" + success + ", resultCode=" + resultCode + ", msg=" + msg
 				+ ", obj=" + obj + ", attributes=" + attributes + "]";
 	}
-
 }
