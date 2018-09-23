3808a0ea548ad71a2308024dea47cce80daf1f25
独泪了无痕 <duleilewuhen@sina.com>
2018-09-23 23:26:14 +0800

    删除

--- wechat-common/src/main/java/com/cdeledu/common/api/email/config/JavaMailType.java
+++
@@ -1,9 +0,0 @@
-package com.cdeledu.common.api.email.config;
-
-public enum JavaMailType {
-	SMTP, /** 简单邮件传输协议（Simple Mail Transfer Protocol） */
-	POP3, /** POP 代表邮局协议（Post Office Protocol） */
-	IMAP, /** 因特网消息访问协议（Internet Message Access Protocol */
-	MIME
-	/** 因特网邮件扩展标准（Multipurpose Internet Mail Extensions） */
-}
