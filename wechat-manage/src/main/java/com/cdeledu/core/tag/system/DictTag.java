f321adcdf32f6884a972e10733914e09d642c4da
独泪了无痕 <duleilewuhen@sina.com>
2018-09-25 14:45:35 +0800

    数字字典

--- wechat-manage/src/main/java/com/cdeledu/core/tag/system/DictTag.java
+++ wechat-manage/src/main/java/com/cdeledu/core/tag/system/DictTag.java
@@ -1,6 +1,7 @@
 package com.cdeledu.core.tag.system;
 
-import com.cdeledu.common.base.BaseClass;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
 
 /**
  * 把今天最好的表现当作明天最新的起点．．～
@@ -10,11 +11,56 @@ import com.cdeledu.common.base.BaseClass;
  * @类描述: 数据字典
  * @创建者: 独泪了无痕--duleilewuhen@sina.com
  * @创建时间: 2017年12月31日 下午8:46:22
- * @版本: V1.0
+ * @版本: V0.0.1
  * @since: JDK 1.7
  */
-public class DictTag extends BaseClass {
-	/** ----------------------------------------------------- Fields start */
+public class DictTag extends TagSupport {
 	private static final long serialVersionUID = 1L;
-	/** ----------------------------------------------------- Fields end */
+	private String id;
+	/** 数据字典的值 */
+	private String value;
+	/** 数据字典的名称 */
+	private String name;
+	/** 是否仅仅显示 */
+	private boolean onlyshow;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isOnlyshow() {
+		return onlyshow;
+	}
+
+	public void setOnlyshow(boolean onlyshow) {
+		this.onlyshow = onlyshow;
+	}
+
+	public int doEndTag() throws JspException {
+		return 0;
+	}
+
+	public int doStartTag() throws JspException {
+		return doEndTag();
+	}
 }
